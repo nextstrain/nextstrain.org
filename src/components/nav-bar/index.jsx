c8226879d9dd3371ccbbaa0352b6434c1f0b5c0a
Trevor Bedford <trevor@bedford.io>
2018-05-07 23:12:08 -0700

    Attempt styled components for navbar

--- src/components/nav-bar/index.jsx
+++ src/components/nav-bar/index.jsx
@@ -1,127 +1,125 @@
 import React from "react";
 import Link from 'gatsby-link';
+import styled from 'styled-components';
 import Flex from "../framework/flex";
 import { titleColors, darkGrey, brandColor } from "../../util/globals";
 import nextstrainLogo from "../../../static/logos/nextstrain-logo-small.png";
 
-class NavBar extends React.Component {
+const NavContainer = styled(Flex)`
+  max-width: 960px;
+  margin-top: auto;
+  margin-right: auto;
+  margin-bottom: auto;
+  margin-left: auto;
+  justify-content: space-between;
+  align-items: center;
+  overflow: hidden;
+  left: 0px;
+  z-index: 1001;
+  transition: left .3s ease-out;
+`;
 
-  getStyles() {
-    return {
-      main: {
-        maxWidth: 960,
-        marginTop: "auto",
-        marginRight: "auto",
-        marginBottom: "auto",
-        marginLeft: "auto",
-        height: 50,
-        justifyContent: "space-between",
-        alignItems: "center",
-        overflow: "hidden",
-        left: 0,
-        zIndex: 1001,
-        transition: "left .3s ease-out"
-      },
-      logo: {
-        paddingLeft: "8px",
-        paddingRight: "8px",
-        paddingTop: "20px",
-        paddingBottom: "20px",
-        color: "#000",
-        cursor: "pointer",
-        textDecoration: "none",
-        fontSize: this.props.minified ? 12 : 16
-      },
-      title: {
-        padding: "0px",
-        color: "#000",
-        textDecoration: "none",
-        fontSize: 20,
-        fontWeight: 400,
-        cursor: "pointer"
-      },
-      link: {
-        paddingLeft: this.props.minified ? "6px" : "12px",
-        paddingRight: this.props.minified ? "6px" : "12px",
-        paddingTop: "20px",
-        paddingBottom: "20px",
-        textDecoration: "none",
-        cursor: "pointer",
-        fontSize: this.props.minified ? 12 : 16,
-        fontWeight: 400,
-        textTransform: "uppercase"
-      },
-      inactive: {
-        paddingLeft: this.props.minified ? "6px" : "12px",
-        paddingRight: this.props.minified ? "6px" : "12px",
-        paddingTop: "20px",
-        paddingBottom: "20px",
-        color: "#5097BA",
-        textDecoration: "none",
-        fontSize: this.props.minified ? 12 : 16,
-        fontWeight: 400,
-        textTransform: "uppercase"
-      },
-      alerts: {
-        textAlign: "center",
-        verticalAlign: "middle",
-        width: 70,
-        color: brandColor
-      }
-    };
-  }
+const NavLogo = styled.div`
+  padding-left: 8px;
+  padding-right: 8px;
+  padding-top: 0px;
+  padding-bottom: 0px;
+  cursor: pointer;
+  font-size: ${(props) => props.minified ? '12px' : '16px'};
+`;
+
+const NavLogoCharacter = styled.span`
+  padding: 0px;
+  text-decoration: none;
+  font-size: 20px;
+  font-weight: 400;
+  cursor: pointer;
+  color: ${(props) => props.color};
+`;
+
+const NavLink = styled(Link)`
+  padding-left: ${(props) => props.minified ? '6px' : '12px'};
+  padding-right: ${(props) => props.minified ? '6px' : '12px'};
+  padding-top: 20px;
+  padding-bottom: 20px;
+  text-decoration: none !important;
+  cursor: pointer;
+  font-size: ${(props) => props.minified ? '12px' : '16px'} !important;
+  font-weight: 400;
+  text-transform: uppercase;
+  color: ${(props) => props.color} !important;
+`;
+
+const NavLinkInactive = styled.div`
+  padding-left: ${(props) => props.minified ? '6px' : '12px'};
+  padding-right: ${(props) => props.minified ? '6px' : '12px'};
+  padding-top: 20px;
+  padding-bottom: 20px;
+  text-decoration: none;
+  font-size: ${(props) => props.minified ? '12px' : '16px'};
+  font-weight: 400;
+  text-transform: uppercase;
+  color: #5097BA;
+`;
+
+class NavBar extends React.Component {
 
   selectedClass(name) {
     if (!this.props.location || !this.props.location.pathname) return "";
     return this.props.location.pathname.startsWith(`/${name}`);
   }
 
-  getLogo(styles) {
+  getLogo() {
     return (
-      <Link to="/" style={styles.logo}>
-        <img alt="Logo" width="40" src={nextstrainLogo}/>
-      </Link>
+      <NavLogo>
+        <Link to="/">
+          <img alt="Logo" width="40" src={nextstrainLogo}/>
+        </Link>
+      </NavLogo>
     );
   }
 
-  getLogoType(styles) {
+  getLogoType() {
     const title = "Nextstrain";
     const rainbowTitle = title.split("").map((letter, i) =>
-      <span key={i} style={{ ...styles.title, ...{color: titleColors[i]} }}>{letter}</span>
+      <NavLogoCharacter key={i} color={titleColors[i]}>{letter}</NavLogoCharacter>
     );
     return (
       this.props.minified ?
         <div/>
         :
-        <Link to="/" style={styles.title}>
+        <Link to="/">
           {rainbowTitle}
         </Link>
+
     );
   }
 
-  getLink(name, url, selected, styles) {
+  getLink(name, url, selected) {
     const linkCol = this.props.minified ? "#000" : darkGrey;
     return (
       selected ?
-        <div style={{ ...{color: linkCol}, ...styles.inactive }}>{name}</div> :
-        <Link to={url} style={{ ...{color: linkCol}, ...styles.link }}>
+        <NavLinkInactive minified={this.props.minified}>
           {name}
-        </Link>
+        </NavLinkInactive>
+        :
+        <NavLink to={url} color={linkCol} minified={this.props.minified}>
+          {name}
+        </NavLink>
     );
   }
 
   render() {
-    const styles = this.getStyles();
     return (
-      <Flex style={styles.main}>
-        {this.getLogo(styles)}
-        {this.getLogoType(styles)}
+      <NavContainer>
+        {this.getLogo()}
+        {this.getLogoType()}
         <div style={{flex: 5}}/>
-        {this.getLink("About", "/about", this.selectedClass("about"), styles)}
-        {this.getLink("Docs", "/docs/builds/zika-build", this.selectedClass("docs"), styles)}
-        {this.getLink("Posts", "/reports/flu-vaccine-selection/2017-february", this.selectedClass("reports"), styles)}
+        {this.getLink("About", "/about", this.selectedClass("about"))}
+        {this.getLink("Docs", "/docs/builds/zika-build", this.selectedClass("docs"))}
+        {this.getLink("Posts", "/reports/flu-vaccine-selection/2017-february", this.selectedClass("reports"))}
         <div style={{width: this.props.minified ? 8 : 0 }}/>
-      </Flex>
+      </NavContainer>
     );
   }
 }
