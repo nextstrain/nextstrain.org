58afa9888bedcc19f02cb1e055f144e0852bbe2e
James Hadfield <jh22@sanger.ac.uk>
2018-05-10 15:03:54 -0700

    Fix heroku-build-specific bugs
    typo
    
    reinstate cloning of auspice so the server can build
    
    dont build server
    
    disable gatsby (speed for debug)
    
    reinstate gatsby. Remove auspice babelrc
    
    set path
    
    relative PATH
    
    AWK
    
    engines

--- build.sh
+++ build.sh
@@ -9,25 +9,36 @@ function errorFound {
 # TRAPS
 trap 'errorFound $LINENO' ERR
 
-
 # Heroku installs the npm libs for this directory only
 # https://devcenter.heroku.com/articles/nodejs-support
 
-echo "Cloning Auspice (branch: server-tidyup) & Static site repos"
-git clone -b server-tidyup --single-branch https://github.com/nextstrain/auspice.git
+echo "Cloning Auspice (branch: no-static) & Static site repos"
+git clone -b no-static --single-branch https://github.com/nextstrain/auspice.git
 git clone -b master --single-branch https://github.com/nextstrain/nextstrain.org.git
+
+# Building Auspice is a nightmare.
 
 echo "Jumping into Auspice"
 cd auspice
 
-echo "installing from npm"
+echo "HACKING PATH"
+OLD_PATH=$PATH
+export PATH=`echo ${PATH} | awk -v RS=: -v ORS=: '/node_modules\/.bin/ {next} {print}'`
+echo "Old Path: ${OLD_PATH}"
+echo "New Path: ${PATH}"
+echo "npm install"
+NODE_ENV=development
 npm install
 
-echo "building auspice (npm run postinstall)"
-npm run postinstall
+echo "building auspice (npm run build)"
+npm run build
 
-echo "Jumping into the Static site"
-cd ../nextstrain.org
+echo "Jumping back to parent directory & resetting PATH"
+cd ..
+env PATH=${OLD_PATH}
+
+echo "into the Static site"
+cd nextstrain.org
 
 echo "installing from npm"
 npm install
@@ -37,6 +48,7 @@ npm run build
 
 echo "building the server"
 cd ..
+rm auspice/.babelrc # why? you get errors because or node_module pathing and babelrc defined plugins. I don't understand why /auspice/.babelrc is even being looked at!
 npm run build
 
 echo "DONE :) Now the Procfile will run the server"
