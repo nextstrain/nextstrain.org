18c113a78a62556789118f9682f71c466bc422e8
James Hadfield <jh22@sanger.ac.uk>
2018-05-14 16:01:22 -0700

    update favicon import

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ const globals = require("./auspice/src/server/globals");
 // NOTE: order of app.get is first come first serve (https://stackoverflow.com/questions/32603818/order-of-router-precedence-in-express-js)
 const app = express();
 app.set('port', process.env.PORT || 5000);
-app.get("/favicon.png", (req, res) => {
-  res.sendFile(path.join(__dirname, "auspice", "favicon.png"));
-});
+app.use(express.favicon(path.join(__dirname, "auspice", "favicon.png")));
 // redirect www.nextstrain.org to nextstrain.org
 app.use(require('express-naked-redirect')({reverse: true}));
 
