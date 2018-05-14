e4239dcf3eed04cf0abdf84c09e70c318be7b5d5
James Hadfield <jh22@sanger.ac.uk>
2018-05-14 12:14:41 -0700

    remove DOM errors (no <div> or <p> inside <p>)

--- src/components/splash/index.jsx
+++ src/components/splash/index.jsx
@@ -5,7 +5,7 @@ import { generateTiles } from "./cards";
 import Title from "./title";
 import * as Styles from "./styles";
 import { SmallSpacer, MediumSpacer, BigSpacer, HugeSpacer,
-  FlexCenter, FlexGrid, TeamMember } from "../../layouts/generalComponents";
+  FlexCenter, TeamMember } from "../../layouts/generalComponents";
 import { Logos } from "../../components/logos";
 
 class Splash extends React.Component {
@@ -26,7 +26,7 @@ class Splash extends React.Component {
           <Styles.CenteredFocusParagraph>
             Nextstrain is an open-source project to harness the scientific and public health potential of pathogen genome data. We provide a continually-updated view of publicly available data with powerful analytics and visualizations showing pathogen evolution and epidemic spread. Our goal is to aid epidemiological understanding and improve outbreak response.
           </Styles.CenteredFocusParagraph>
-        </FlexCenter> 
+        </FlexCenter>
 
         <BigSpacer/>
 
@@ -64,32 +64,29 @@ class Splash extends React.Component {
         <BigSpacer/>
         <div className="row">
           <div className="col-md-6">
+            <Styles.H2>Viral Phylogenies</Styles.H2>
             <Styles.FocusParagraph>
-              <Styles.H2>Viral Phylogenies</Styles.H2>
-              <MediumSpacer />
               In the course of an infection and over an epidemic, viral pathogens naturally accumulate random mutations to their genomes. This is an inevitable consequence of error-prone viral replication. Since different viruses typically pick up different mutations, mutations can be used as a marker of transmission in which closely related viral genomes indicate closely related infections. By reconstructing a viral <i>phylogeny</i> we can learn about important epidemiological phenomena such as spatial spread, introduction timings and epidemic growth rate.
             </Styles.FocusParagraph>
           </div>
           <div className="col-md-6">
+            <Styles.H2>Actionable Inferences</Styles.H2>
             <Styles.FocusParagraph>
-              <Styles.H2>Actionable Inferences</Styles.H2>
-              <MediumSpacer />
               However, if viral genome sequences are going to inform public health interventions, then analyses have to be rapidly conducted and results widely disseminated. Current scientific publishing practices hinder the rapid dissemination of epidemiologically relevant results. We thought an open online system that implements robust bioinformatic pipelines to synthesize data from across research groups has the best capacity to make epidemiologically actionable inferences.
             </Styles.FocusParagraph>
           </div>
         </div>
+        <BigSpacer/>
         <div className="row">
           <div className="col-md-6">
+            <Styles.H2>This Website</Styles.H2>
             <Styles.FocusParagraph>
-              <Styles.H2>This Website</Styles.H2>
-              <MediumSpacer />
               This website aims to provide a <i>real-time</i> snapshot of evolving viral populations and to provide interactive data visualizations to virologists, epidemiologists, public health officials and citizen scientists. Through interactive data visualizations, we aim to allow exploration of continually up-to-date datasets, providing a novel surveillance tool to the scientific and public health communities.
             </Styles.FocusParagraph>
           </div>
           <div className="col-md-6">
+            <Styles.H2>Future Directions</Styles.H2>
             <Styles.FocusParagraph>
-              <Styles.H2>Future Directions</Styles.H2>
-              <MediumSpacer />
               Nextstrain is under active development and we have big plans for its future, including visualization, bioinformatics analysis and an increasing number and variety of datasets. Please get in touch with <a href="https://twitter.com/hamesjadfield">@hamesjadfield</a>, <a href="https://twitter.com/richardneher">@richardneher</a> or <a href="https://twitter.com/trvrb">@trvrb</a> with any questions or comments.
             </Styles.FocusParagraph>
           </div>
@@ -112,12 +109,11 @@ class Splash extends React.Component {
         <div className="row">
           <div className="col-md-12">
             <Styles.WideParagraph>
-              <p>
-                All <a href="https://github.com/nextstrain">source code</a> is freely available under the terms of the <a href="https://github.com/nextstrain/auspice/blob/master/LICENSE.txt">GNU Affero General Public License</a>. Screenshots etc may be used as long as a link to nextstrain.org is provided.
-              </p>
-              <p>
-                This work is made possible by the open sharing of genetic data by research groups from all over the world. We gratefully acknowledge their contributions. Special thanks to Kristian Andersen, Allison Black, David Blazes, Peter Bogner, Matt Cotten, Ana Crisan, Gytis Dudas, Vivien Dugan, Karl Erlandson, Nuno Faria, Jennifer Gardy, Becky Garten, Dylan George, Ian Goodfellow, Nathan Grubaugh, Betz Halloran, Christian Happi, Jeff Joy, Paul Kellam, Philippe Lemey, Nick Loman, Sebastian Maurer-Stroh, Louise Moncla, Oliver Pybus, Andrew Rambaut, Colin Russell, Pardis Sabeti, Katherine Siddle, Kristof Theys, Dave Wentworth, Shirlee Wohl and Nathan Yozwiak for comments, suggestions and data sharing.
-              </p>
+              All <a href="https://github.com/nextstrain">source code</a> is freely available under the terms of the <a href="https://github.com/nextstrain/auspice/blob/master/LICENSE.txt">GNU Affero General Public License</a>. Screenshots etc may be used as long as a link to nextstrain.org is provided.
+            </Styles.WideParagraph>
+
+            <Styles.WideParagraph>
+              This work is made possible by the open sharing of genetic data by research groups from all over the world. We gratefully acknowledge their contributions. Special thanks to Kristian Andersen, Allison Black, David Blazes, Peter Bogner, Matt Cotten, Ana Crisan, Gytis Dudas, Vivien Dugan, Karl Erlandson, Nuno Faria, Jennifer Gardy, Becky Garten, Dylan George, Ian Goodfellow, Nathan Grubaugh, Betz Halloran, Christian Happi, Jeff Joy, Paul Kellam, Philippe Lemey, Nick Loman, Sebastian Maurer-Stroh, Louise Moncla, Oliver Pybus, Andrew Rambaut, Colin Russell, Pardis Sabeti, Katherine Siddle, Kristof Theys, Dave Wentworth, Shirlee Wohl and Nathan Yozwiak for comments, suggestions and data sharing.
             </Styles.WideParagraph>
           </div>
         </div>
