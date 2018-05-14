76088281713c2f9ce3d40b5162de556f9428684c
James Hadfield <jh22@sanger.ac.uk>
2018-05-14 15:07:56 -0700

    minor style tweaks

--- src/components/splash/index.jsx
+++ src/components/splash/index.jsx
@@ -5,7 +5,7 @@ import { generateTiles } from "./cards";
 import Title from "./title";
 import * as Styles from "./styles";
 import { SmallSpacer, MediumSpacer, BigSpacer, HugeSpacer,
-  FlexCenter, TeamMember } from "../../layouts/generalComponents";
+  FlexCenter, TeamMember, Line } from "../../layouts/generalComponents";
 import { Logos } from "../../components/logos";
 
 class Splash extends React.Component {
@@ -61,30 +61,32 @@ class Splash extends React.Component {
         <ScrollableAnchor id={'philosophy'}>
           <Styles.H1>Philosophy</Styles.H1>
         </ScrollableAnchor>
-        <BigSpacer/>
         <div className="row">
           <div className="col-md-6">
+            <BigSpacer/>
             <Styles.H2>Viral Phylogenies</Styles.H2>
             <Styles.FocusParagraph>
               In the course of an infection and over an epidemic, viral pathogens naturally accumulate random mutations to their genomes. This is an inevitable consequence of error-prone viral replication. Since different viruses typically pick up different mutations, mutations can be used as a marker of transmission in which closely related viral genomes indicate closely related infections. By reconstructing a viral <i>phylogeny</i> we can learn about important epidemiological phenomena such as spatial spread, introduction timings and epidemic growth rate.
             </Styles.FocusParagraph>
           </div>
           <div className="col-md-6">
+            <BigSpacer/>
             <Styles.H2>Actionable Inferences</Styles.H2>
             <Styles.FocusParagraph>
               However, if viral genome sequences are going to inform public health interventions, then analyses have to be rapidly conducted and results widely disseminated. Current scientific publishing practices hinder the rapid dissemination of epidemiologically relevant results. We thought an open online system that implements robust bioinformatic pipelines to synthesize data from across research groups has the best capacity to make epidemiologically actionable inferences.
             </Styles.FocusParagraph>
           </div>
         </div>
-        <BigSpacer/>
         <div className="row">
           <div className="col-md-6">
+            <BigSpacer/>
             <Styles.H2>This Website</Styles.H2>
             <Styles.FocusParagraph>
               This website aims to provide a <i>real-time</i> snapshot of evolving viral populations and to provide interactive data visualizations to virologists, epidemiologists, public health officials and citizen scientists. Through interactive data visualizations, we aim to allow exploration of continually up-to-date datasets, providing a novel surveillance tool to the scientific and public health communities.
             </Styles.FocusParagraph>
           </div>
           <div className="col-md-6">
+            <BigSpacer/>
             <Styles.H2>Future Directions</Styles.H2>
             <Styles.FocusParagraph>
               Nextstrain is under active development and we have big plans for its future, including visualization, bioinformatics analysis and an increasing number and variety of datasets. Please get in touch with <a href="https://twitter.com/hamesjadfield">@hamesjadfield</a>, <a href="https://twitter.com/richardneher">@richardneher</a> or <a href="https://twitter.com/trvrb">@trvrb</a> with any questions or comments.
@@ -92,7 +94,7 @@ class Splash extends React.Component {
           </div>
         </div>
 
-        <BigSpacer/>
+        <Line style={{margin: "30px 0px 10px 0px"}}/>
 
         <div className="row">
           <div className="col-md-1"/>
