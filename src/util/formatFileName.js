77d4baab96dd26d2ac0f009af7331e6388066c0b
James Hadfield <jh22@sanger.ac.uk>
2018-05-14 16:39:13 -0700

    strip out "YYYY-MM-DD-" for sidebar display but not for the URL

--- src/util/formatFileName.js
+++ src/util/formatFileName.js
@@ -1,9 +1,10 @@
 const _ = require("lodash");
 
 export const formatFileName = (name) => {
-  const parts = /^\d\d-(.*)/.exec(name);
+  /* strip out the YYYY-MM-DD- bit from blog posts for sidebar display but not for the URL */
+  const parts = /^\d{4}-\d{2}-\d{2}-(.*)/.exec(name);
   if (parts) {
-    return _.startCase(parts[1])
+    return _.startCase(parts[1]);
   }
-  return _.startCase(name)
-}
+  return _.startCase(name);
+};
