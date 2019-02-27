5bcd8d5d57a88062fdc1c9ee02aaf3bf0d68cb0d
james hadfield <jh22@sanger.ac.uk>
2019-02-27 13:22:19 +0545

    Update auspice to v1.36.1 & instructions on how to install from github

--- set-up.sh
+++ set-up.sh
@@ -19,8 +19,15 @@ npm run build # build using gatsby. Can take a few minutes.
 cd ..
 
 ##############################################################
-echo "Step 2: Building a customised version of auspice"
-npm install --global auspice@^1.36.0
+echo "Step 2a: Installing auspice (globally)"
+npm install --global auspice@1.36.1
+
+# for testing reasons (e.g. deploying to dev-heroku server) you may wish to
+# install from a github branch (useful for auspice versions not pushed to npm)
+# npm install --global https://github.com/nextstrain/auspice/tarball/master
+
+
+echo "Step 2b: Building a customised version of auspice"
 cd auspice
 auspice build --verbose --extend ./client/config.json
 cd ..
