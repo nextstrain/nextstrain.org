05a211fce276902ca1c8e84dbd32adc7f4eda9f5
Trevor Bedford <trevor@bedford.io>
2019-07-26 15:54:31 -0700

    Tweak splash page
    1. Add link to docs/contributing/community-builds/
    2. Tweak spacers
    3. Reflow text for readability

--- static-site/src/components/Footer/index.jsx
+++ static-site/src/components/Footer/index.jsx
@@ -86,7 +86,7 @@ class Footer extends React.Component {
         <div className="row">
           <div className="col-md-12">
             <Styles.FooterParagraph>
-              © 2015-2018 Trevor Bedford and Richard Neher
+              © 2015-2019 Trevor Bedford and Richard Neher
             </Styles.FooterParagraph>
           </div>
         </div>
