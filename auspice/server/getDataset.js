5e76c8c9a1fd836187155197d279c00dd042f8e6
Kairsten Fay <kairsten.fay@gmail.com>
2019-08-26 16:41:21 -0700

    Refactor `getDataset.js`
    Improve readability of the code in the file `getDataset.js` by more
    explicit naming, breaking down of long functions into smaller functions,
    and reducing indendation.

--- auspice/server/getDataset.js
+++ auspice/server/getDataset.js
@@ -2,16 +2,7 @@ const queryString = require("query-string");
 const utils = require("./utils");
 const helpers = require("./getDatasetHelpers");
 
-
-const getDataset = async (req, res) => {
-  const query = queryString.parse(req.url.split('?')[1]);
-  if (!query.prefix) {
-    return helpers.handleError(res, `getDataset request must define a prefix`);
-  }
-  const source = helpers.decideSourceFromPrefix(query.prefix);
-  utils.log(`Getting (nextstrain) datasets for: ${req.url.split('?')[1]}. Source: ${source}`);
-
-  // construct fetch URL
+const constructFetchUrl = (res, source, query) => {
   let datasetInfo;
   try {
     datasetInfo = helpers.parsePrefix(source, query.prefix, query);
@@ -19,42 +10,78 @@ const getDataset = async (req, res) => {
     // utils.printStackTrace(err);
     return helpers.handleError(res, `Couldn't parse the url "${query.prefix}" for source "${source}"`, err.message);
   }
+  return datasetInfo;
+};
 
-  /* Are we requesting a certain file type? */
+/**
+ *
+ * @param {*} res
+ * @param {*} datasetInfo
+ * @param {*} query
+ */
+const requestCertainFileType = async (res, req, datasetInfo, query) => {
+  const jsonData = await utils.fetchJSON(datasetInfo.fetchUrls.additional);
+  if (query.type === "tree") {
+    res.json({tree: jsonData});
+  }
+  res.json(jsonData);
+};
+
+/**
+ * Currently the main datasets are tree + meta
+ *
+ * @param {*} datasetInfo
+ * @param {*} query
+ */
+const requestMainDataset = async (res, req, datasetInfo, source) => {
+  const fetchMultiple = [utils.fetchJSON(datasetInfo.fetchUrls.meta),
+    utils.fetchJSON(datasetInfo.fetchUrls.tree)];
+
+  if (datasetInfo.fetchUrls.secondTree) {
+    fetchMultiple.push(utils.fetchJSON(datasetInfo.fetchUrls.secondTree));
+  }
+
+  const data = await Promise.all(fetchMultiple);
+
+  const jsonData = {
+    meta: data[0],
+    tree: data[1],
+    _source: source,
+    _treeName: datasetInfo.treeName
+  };
+
+  if (datasetInfo.fetchUrls.secondTree) {
+    jsonData._treeTwoName = datasetInfo.secondTreeName;
+    jsonData.treeTwo = data[2];
+  }
+
+  utils.verbose(`Success fetching ${fetchMultiple.length} version 1 auspice JSONs. Sending as a single JSON.`);
+  res.send(jsonData);
+};
+
+const getDataset = async (req, res) => {
+  const query = queryString.parse(req.url.split('?')[1]);
+  if (!query.prefix) {
+    return helpers.handleError(res, `getDataset request must define a prefix`);
+  }
+
+  const source = helpers.decideSourceFromPrefix(query.prefix);
+  const datasetInfo = constructFetchUrl(res, source, query);
+
+  utils.log(`Getting (nextstrain) datasets for: ${req.url.split('?')[1]}. Source: ${source}`);
+
   if (datasetInfo.fetchUrls.additional) {
     try {
-      const jsonData = await utils.fetchJSON(datasetInfo.fetchUrls.additional);
-      if (query.type === "tree") {
-        return res.json({tree: jsonData});
-      }
-      return res.json(jsonData);
+      await requestCertainFileType(res, req, datasetInfo, query);
     } catch (err) {
       return helpers.handleError(res, `Couldn't fetch JSON: ${datasetInfo.fetchUrls.additional}`, err.message);
     }
-  }
-
-  /* request the main dataset (currently tree + meta) */
-  try {
-    const fetchMultiple = [utils.fetchJSON(datasetInfo.fetchUrls.meta), utils.fetchJSON(datasetInfo.fetchUrls.tree)];
-    if (datasetInfo.fetchUrls.secondTree) {
-      fetchMultiple.push(utils.fetchJSON(datasetInfo.fetchUrls.secondTree));
+  } else {
+    try {
+      await requestMainDataset(res, req, datasetInfo, source);
+    } catch (err) {
+      return helpers.handleError(res, `Couldn't fetch JSONs`, err.message);
     }
-    const data = await Promise.all(fetchMultiple);
-    const jsonData = {
-      meta: data[0],
-      tree: data[1],
-      _source: source,
-      _treeName: datasetInfo.treeName,
-      _url: datasetInfo.auspiceDisplayUrl
-    };
-    if (datasetInfo.fetchUrls.secondTree) {
-      jsonData._treeTwoName = datasetInfo.secondTreeName;
-      jsonData.treeTwo = data[2];
-    }
-    utils.verbose(`Success fetching ${fetchMultiple.length} version 1 auspice JSONs. Sending as a single JSON.`);
-    res.json(jsonData);
-  } catch (err) {
-    return helpers.handleError(res, `Couldn't fetch JSONs`, err.message);
   }
   return undefined;
 };
