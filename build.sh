5ea06ea87208ce29c2ef55614c9d1d89251810e1
james hadfield <jh22@sanger.ac.uk>
2019-10-30 11:10:42 +1300

    update auspice to 2.0.2

--- build.sh
+++ build.sh
@@ -41,7 +41,7 @@ build-static() {
 
 build-auspice() {
     echo "Installing auspice (globally)"
-    npm install --global auspice@2.0.1
+    npm install --global auspice@2.0.2
     
     # for testing reasons (e.g. deploying to dev-heroku server) you may wish to
     # install from a github branch (useful for auspice versions not pushed to npm)
