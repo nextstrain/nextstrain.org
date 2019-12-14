6019dabb974c621ddb914d3e72ad934cc6fe1f9a
Kairsten Fay <kairsten.fay@gmail.com>
2019-12-13 16:28:21 -0800

    WIP Style the new Users page
    Add the navigation bar (navbar), logos footer to the new `/users/` page.
    Format the main message with CSS and spacing in-between group names.

--- static-site/src/pages/users.jsx
+++ static-site/src/pages/users.jsx
@@ -1,20 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import NavBar from '../components/nav-bar';
+import { Logos } from "../components/logos";
 import withLocation from "../templates/withLocation";
 
 const UsersPage = ({ search }) => {
   const { user, groups } = search;
   console.log(search);
   return (
-    !!user
-      ? <LoggedIn search={search} />
-      : <LoggedOut />
+    <div className="index-container">
+      <main>
+        <NavBar minified/>
+        <UserContainer>
+          {!!user
+            ? <LoggedIn search={search} />
+            : <LoggedOut />}
+        </UserContainer>
+        <Logos />
+      </main>
+    </div>
   );
 };
 
 const LoggedIn = (props) => (
   <p>
     You&apos;re logged in as <strong>{props.search.user}</strong>.<br />
-    You are in the groups <strong>{props.search.groups}</strong>.<br />
+    You are in the groups <strong>{props.search.groups.split(',').join(', ')}</strong>.<br />
     <a href="/logout">Logout</a>
   </p>
 );
@@ -25,5 +36,18 @@ const LoggedOut = () => (
     <a href="/login">Login</a>
   </p>
 );
+
+const UserContainer = styled.div`
+  max-width: 640px;
+  padding: 130px 0px 80px 0px;
+  margin-top: 0px;
+  margin-right: auto;
+  margin-bottom: 0px;
+  margin-left: auto;
+  text-align: center;
+  font-size: 28px;
+  font-weight: 300;
+  line-height: ${(props) => 1.4 * props.theme.niceLineHeight};
+`;
 
 export default withLocation(UsersPage);
