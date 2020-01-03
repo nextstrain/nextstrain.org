8da4a4b60b6edf3cca6ed735b900bded485e372c
Jover <joverlee521@gmail.com>
2020-01-02 16:41:50 -0800

    gatsby: Add userDataWrapper to layouts
    Provides a wrapper that fetches user data from the API endpoint
    "/whoami". Passes the user state to all children through the prop `user`

---
+++ static-site/src/layouts/userDataWrapper.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default class UserDataWrapper extends React.Component {
+  state = {
+    user: undefined
+  }
+
+  componentDidMount() {
+    this.loadUser();
+  }
+
+  loadUser() {
+    console.log("Fetching user data");
+    fetch("/whoami", { headers: { Accept: 'application/json' }})
+      .then((response) => response.json())
+      .then((whoami) => this.setState((state) => ({...state, ...whoami})));
+  }
+
+  render() {
+    const { children } = this.props;
+    const childElements = React.Children.map(children, child =>
+      // Passes the user state as a prop to all children elements via the prop `user`
+      child && React.cloneElement(child, {
+        user: this.state.user
+      })
+    );
+    return <div>{ childElements }</div>;
+  }
+}
