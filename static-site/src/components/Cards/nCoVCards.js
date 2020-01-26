16f516777ab327d8e00aabe73a8e7c1404cd418c
Richard Neher <richard.neher@unibas.ch>
2020-01-26 22:01:40 +0100

    Chinese Language situation report

--- static-site/src/components/Cards/nCoVCards.js
+++ static-site/src/components/Cards/nCoVCards.js
@@ -8,6 +8,11 @@ const nCoVCards = [
     img: "ncov_narrative.png",
     url: "/narratives/ncov/sit-rep/2020-01-25",
     title: "Situation Report 2020-01-25"
+  },
+  {
+    img: "ncov_narrative.png",
+    url: "/narratives/ncov/sit-rep/zh/2020-01-25",
+    title: "状况报告2020-01-25。"
   }
 ];
 
