b99e8557d540dd085ed25b072bdaee6e7aa712b2
cassiawag <41387591+cassiawag@users.noreply.github.com>
2020-03-15 09:57:38 -0600

    Updates Mandarin tile

--- static-site/src/components/Cards/nCoVCards.js
+++ static-site/src/components/Cards/nCoVCards.js
@@ -11,8 +11,8 @@ const nCoVCards = [
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/zh/2020-03-05",
-    title: "状况报告2020-03-05"
+    url: "/narratives/ncov/sit-rep/zh/2020-03-13",
+    title: "状况报告2020-03-13"
   },
   {
     img: "ncov_narrative.png",
