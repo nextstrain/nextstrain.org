8122b58cd8aa5867b437f36624477d3824ed9de8
cassiawag <41387591+cassiawag@users.noreply.github.com>
2020-03-15 00:29:30 -0600

    Updates German card

--- static-site/src/components/Cards/nCoVCards.js
+++ static-site/src/components/Cards/nCoVCards.js
@@ -36,8 +36,8 @@ const nCoVCards = [
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/de/2020-03-05",
-    title: "Lagebericht 2020-03-05"
+    url: "/narratives/ncov/sit-rep/de/2020-03-13",
+    title: "Lagebericht 2020-03-13"
   },
   {
     img: "ncov_narrative.png",
