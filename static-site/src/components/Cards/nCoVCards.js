f03c51b599111084532893af3184b6317d2199d4
Cassia Wagner <cassia.wagner9@gmail.com>
2020-03-19 18:15:51 -0700

    Adds Polish tile; removes Hindi tile

--- static-site/src/components/Cards/nCoVCards.js
+++ static-site/src/components/Cards/nCoVCards.js
@@ -67,12 +67,12 @@ const nCoVCards = [
   {
     img: "ncov_narrative.png",
     url: "/narratives/ncov/sit-rep/ar/2020-03-13",
-    title: ".تقرير حالة 13 مارس 2020"
+    title: "تقرير حالة 13 مارس 2020"
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/hi/2020-03-05",
-    title: "परिस्थिति रिपोर्ट 2020-03-05"
+    url: "/narratives/ncov/sit-rep/pl/2020-03-13",
+    title: "Raport sytuacyjny 2020-03-13"
   }
 ];
 
