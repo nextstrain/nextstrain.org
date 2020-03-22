a08b55223fae801b7256e4ee7c81c00c0885cfe3
cassiawag <41387591+cassiawag@users.noreply.github.com>
2020-03-22 12:06:31 -0600

    Update Japanese tile

--- static-site/src/components/Cards/nCoVCards.js
+++ static-site/src/components/Cards/nCoVCards.js
@@ -51,8 +51,8 @@ const nCoVCards = [
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/ja/2020-03-13",
-    title: "状況報告 2020-03-13"
+    url: "/narratives/ncov/sit-rep/ja/2020-03-20",
+    title: "状況報告 2020-03-20"
   },
   {
     img: "ncov_narrative.png",
