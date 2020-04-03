bd1c4fced71ec101d5be0838fd028449c49089bd
Philip Thomas <pthomas.v3@gmail.com>
2020-04-03 16:15:35 -0500

    Fix siteLogo url
    The open graph image in the meta tags 404s because the siteLogo url in
    SiteConfig.js points to the location in the repo rather than the public
    location after the static site is built.

--- static-site/data/SiteConfig.js
+++ static-site/data/SiteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteTitle: "Nextstrain",
   siteTitleAlt: "Real-time tracking of pathogen evolution", // Alternative site title for SEO.
-  siteLogo: "/static/logos/nextstrain-logo-small.png", // Logo used for SEO
+  siteLogo: "/logos/nextstrain-logo-small.png", // Logo used for SEO
   siteUrl: "https://nextstrain.org",
   pathPrefix: "/",
   siteDescription: "Real-time tracking of pathogen evolution", // Website description used for RSS feeds/meta description tag.
