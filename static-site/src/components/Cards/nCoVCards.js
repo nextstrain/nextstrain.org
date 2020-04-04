a67e00ef40df143b56e702ab34123ae68c9101e0
cassiawag <41387591+cassiawag@users.noreply.github.com>
2020-04-04 09:48:43 -0600

    Updates Portuguese tile

--- static-site/src/components/Cards/nCoVCards.js
+++ static-site/src/components/Cards/nCoVCards.js
@@ -26,8 +26,8 @@ const nCoVCards = [
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/pt/2020-03-20",
-    title: "Relatório da situação até 2020-03-20"
+    url: "/narratives/ncov/sit-rep/pt/2020-03-27",
+    title: "Relatório da situação até 2020-03-27"
   },
   {
     img: "ncov_narrative.png",
