4a05d433196468a8d4a89e3cb00a7515c2e3274e
cassiawag <41387591+cassiawag@users.noreply.github.com>
2020-04-04 18:01:49 -0600

    Update Arabic tile

--- static-site/src/components/Cards/nCoVCards.js
+++ static-site/src/components/Cards/nCoVCards.js
@@ -66,8 +66,8 @@ const nCoVCards = [
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/ar/2020-03-27",
-    title: "تقرير حالة 27 مارس 2020"
+    url: "/narratives/ncov/sit-rep/ar/2020-04-03",
+    title: " تقرير حالة 3 نيسان 2020"
   },
   {
     img: "ncov_narrative.png",
