7f6b927fe90a0ffa97bc2ea5f21bc6c738e2729c
cassiawag <41387591+cassiawag@users.noreply.github.com>
2020-04-26 10:08:08 -0600

    Updates Indonesian tile

--- static-site/src/components/Cards/nCoVCards.js
+++ static-site/src/components/Cards/nCoVCards.js
@@ -116,8 +116,8 @@ const nCoVCards = [
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/id/2020-04-17",
-    title: "Laporan keadaan per 2020-04-17"
+    url: "/narratives/ncov/sit-rep/id/2020-04-24",
+    title: "Laporan keadaan per 2020-04-24"
   },
   {
     img: "ncov_narrative.png",
