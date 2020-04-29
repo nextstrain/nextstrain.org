9f1c9999500aef293189d835decec2124dc91dc5
james hadfield <jh22@sanger.ac.uk>
2020-04-29 16:40:05 +1200

    Add functionality to list all ncov situation reports
    Styling, error handling not implemented

--- static-site/src/pages/ncov-sit-reps.jsx
+++ static-site/src/pages/ncov-sit-reps.jsx
@@ -3,9 +3,24 @@ import Helmet from "react-helmet";
 import config from "../../data/SiteConfig";
 import NavBar from '../components/nav-bar';
 import MainLayout from "../components/layout";
+import { FlexCenter } from "../layouts/generalComponents";
+import * as Styles from "../components/splash/styles";
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Index extends React.Component {
+
+  componentDidMount() {
+    fetch(`/charon/getAvailable`)
+      .then((res) => res.json())
+      .then((json) => {
+        const narratives = json.narratives
+          .map((o) => "/"+o.request)
+          .filter((o) => o.startsWith("/narratives/ncov/sit-rep/"));
+        this.setState({ narratives });
+      });
+    // todo: graceful error handling
+  }
+
   render() {
     return (
       <MainLayout>
@@ -13,7 +28,32 @@ class Index extends React.Component {
           <Helmet title={config.siteTitle} />
           <main>
             <NavBar location={this.props.location} />
-            Placeholder for listing of SARS-CoV-2 situation reports
+
+            {this.state && this.state.narratives ? (
+              <FlexCenter>
+                <Styles.CenteredFocusParagraph>
+                  All ncov situation reports:
+
+                  {this.state.narratives.map((url) => {
+                    // here we're relying on the URLs all adhering to an unspoken scheme
+                    // todo: make this more robust (catch errors)
+                    const parts = url.replace("/narratives/ncov/sit-rep/", "").split("/");
+                    let name;
+                    if (parts.length === 1) {
+                      name = parts[0];
+                    } else if (parts.length === 2) {
+                      name = `${parts[1]} (${parts[0].toUpperCase()})`;
+                    } else {
+                      console.warn("Unforseen narrative url", url);
+                      return null;
+                    }
+                    return (<p><a href={url}>{name}</a></p>);
+                  })}
+
+                </Styles.CenteredFocusParagraph>
+              </FlexCenter>
+            ) : null}
+
           </main>
         </div>
       </MainLayout>
