5d9e81aeb0640318735aba2b9697e4a121279e25
cassiawag <41387591+cassiawag@users.noreply.github.com>
2020-05-02 17:34:20 -0600

    update Russian tile

--- static-site/src/components/Cards/nCoVCards.js
+++ static-site/src/components/Cards/nCoVCards.js
@@ -86,8 +86,8 @@ const nCoVCards = [
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/ru/2020-04-24",
-    title: "Ситуационный отчет 2020-04-24"
+    url: "/narratives/ncov/sit-rep/ru/2020-05-01",
+    title: "Ситуационный отчет 2020-05-01"
   },
   {
     img: "ncov_narrative.png",
