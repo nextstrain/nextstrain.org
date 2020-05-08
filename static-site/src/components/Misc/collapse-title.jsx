4224913aa36c41f3813300d718c1039ce53e0e5a
eharkins <eli.harkins@gmail.com>
2020-05-08 12:35:46 -0700

    add back in icon font size

--- static-site/src/components/Misc/collapse-title.jsx
+++ static-site/src/components/Misc/collapse-title.jsx
@@ -10,16 +10,18 @@ const TitleContainer = styled.div`
   margin: 0px;
   padding: 15px 10px 10px 10px;
 `;
-
+const IconContainer = styled.span`
+  font-size: 22px;
+  font-weight: 500;
+`;
 const Name = styled.span`
   font-size: 20px;
   font-weight: 500;
 `;
-
 const CollapseTitle = ({name, isExpanded=false}) => (
   <TitleContainer>
     <Name>{name}</Name>
-    {isExpanded ? <FaAngleUp /> : <FaAngleDown />}
+    <IconContainer>{isExpanded ? <FaAngleUp /> : <FaAngleDown />}</IconContainer>
   </TitleContainer>
 );
 
