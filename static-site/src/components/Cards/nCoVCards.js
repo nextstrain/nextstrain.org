a1eedad403105ffc782386a9fdf3741a85bc14c3
Cassia Wagner <cassia.wagner9@gmail.com>
2020-05-10 12:08:11 -0700

    Adds Bengali, hindi, swahili, vietnamese, turkish, greek, czech May 8 cards

--- static-site/src/components/Cards/nCoVCards.js
+++ static-site/src/components/Cards/nCoVCards.js
@@ -41,28 +41,18 @@ const nCoVCards = [
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/zh/2020-05-01",
-    title: "状况报告2020-05-01"
+    url: "/narratives/ncov/sit-rep/ar/2020-05-08",
+    title: "تقرير حالة 8 أيار 2020"
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/it/2020-05-01",
-    title: "Rapporto sulla situazione al 2020-05-01"
+    url: "/narratives/ncov/sit-rep/bn/2020-05-08",
+    title: "পরিস্থিতি সম্পর্কে রিপোর্ট 2020-05-08"
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/es/2020-05-01",
-    title: "Informe de la situación hasta 2020-05-01"
-  },
-  {
-    img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/pt/2020-05-08",
-    title: "Relatório da situação até 2020-05-08"
-  },
-  {
-    img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/fr/2020-05-08",
-    title: "Rapport de situation 2020-05-08"
+    url: "/narratives/ncov/sit-rep/cs/2020-05-08",
+    title: "Situační zpráva ze dne 08. 5. 2020."
   },
   {
     img: "ncov_narrative.png",
@@ -71,8 +61,13 @@ const nCoVCards = [
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/nl/2020-05-08",
-    title: "Situatierapportage 2020-05-08"
+    url: "/narratives/ncov/sit-rep/el/2020-05-08",
+    title: "Κατάσταση 2020-05-01"
+  },
+  {
+    img: "ncov_narrative.png",
+    url: "/narratives/ncov/sit-rep/es/2020-05-01",
+    title: "Informe de la situación hasta 2020-05-01"
   },
   {
     img: "ncov_narrative.png",
@@ -81,38 +76,13 @@ const nCoVCards = [
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/ja/2020-05-01",
-    title: "状況報告 2020-05-01"
+    url: "/narratives/ncov/sit-rep/fr/2020-05-08",
+    title: "Rapport de situation 2020-05-08"
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/ru/2020-05-08",
-    title: "Ситуационный отчет 2020-05-08"
-  },
-  {
-    img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/ko/2020-04-24",
-    title: "2020년 4월 24일 상황 보고서"
-  },
-  {
-    img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/ar/2020-05-08",
-    title: "تقرير حالة 8 أيار 2020"
-  },
-  {
-    img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/tr/2020-05-01",
-    title: "2020-05-01 tarihli durum raporu"
-  },
-  {
-    img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/el/2020-05-01",
-    title: "Κατάσταση 2020-05-01"
-  },
-  {
-    img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/pl/2020-05-08",
-    title: "Raport sytuacyjny 2020-05-08"
+    url: "/narratives/ncov/sit-rep/hi/2020-05-08",
+    title: "स्थिति रिपोर्ट 2020-05-08"
   },
   {
     img: "ncov_narrative.png",
@@ -121,8 +91,58 @@ const nCoVCards = [
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/cs/2020-05-01",
-    title: "Situační zpráva ze dne 01. 5. 2020."
+    url: "/narratives/ncov/sit-rep/it/2020-05-01",
+    title: "Rapporto sulla situazione al 2020-05-01"
+  },
+  {
+    img: "ncov_narrative.png",
+    url: "/narratives/ncov/sit-rep/ja/2020-05-01",
+    title: "状況報告 2020-05-01"
+  },
+  {
+    img: "ncov_narrative.png",
+    url: "/narratives/ncov/sit-rep/ko/2020-04-24",
+    title: "2020년 4월 24일 상황 보고서"
+  },
+  {
+    img: "ncov_narrative.png",
+    url: "/narratives/ncov/sit-rep/nl/2020-05-08",
+    title: "Situatierapportage 2020-05-08"
+  },
+  {
+    img: "ncov_narrative.png",
+    url: "/narratives/ncov/sit-rep/pl/2020-05-08",
+    title: "Raport sytuacyjny 2020-05-08"
+  },
+  {
+    img: "ncov_narrative.png",
+    url: "/narratives/ncov/sit-rep/pt/2020-05-08",
+    title: "Relatório da situação até 2020-05-08"
+  },
+  {
+    img: "ncov_narrative.png",
+    url: "/narratives/ncov/sit-rep/ru/2020-05-08",
+    title: "Ситуационный отчет 2020-05-08"
+  },
+  {
+    img: "ncov_narrative.png",
+    url: "/narratives/ncov/sit-rep/sw/2020-05-08",
+    title: "Ripoti ya 2020-05-08 kuhusu hali ilivyo"
+  },
+  {
+    img: "ncov_narrative.png",
+    url: "/narratives/ncov/sit-rep/tr/2020-05-08",
+    title: "2020-05-08 tarihli durum raporu"
+  },
+  {
+    img: "ncov_narrative.png",
+    url: "/narratives/ncov/sit-rep/vi/2020-05-08",
+    title: "Báo cáo hiện trạng 08-05-2020"
+  },
+  {
+    img: "ncov_narrative.png",
+    url: "/narratives/ncov/sit-rep/zh/2020-05-01",
+    title: "状况报告2020-05-01"
   }
 ];
 
