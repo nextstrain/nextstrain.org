fb49e674905e8e8178c86a380df0d46bd7782fad
Trevor Bedford <trevor@bedford.io>
2020-06-03 19:50:37 -0700

    Improve ncov splash page links
    Link out to preferred viz state for different builds, ie Oceania should be division coloring and global should be region coloring, etc...

--- static-site/src/components/Cards/nCoVCards.js
+++ static-site/src/components/Cards/nCoVCards.js
@@ -1,37 +1,37 @@
 const nCoVCards = [
   {
     img: "ncov.png",
-    url: "/ncov/global",
+    url: "/ncov/global?c=region",
     title: "Latest global analysis"
   },
   {
     img: "ncov.png",
-    url: "/ncov/north-america",
+    url: "/ncov/north-america?c=division&f_region=North%20America&r=division",
     title: "Latest analysis for North America"
   },
   {
     img: "ncov.png",
-    url: "/ncov/europe",
+    url: "/ncov/europe?f_region=Europe",
     title: "Latest analysis for Europe"
   },
   {
     img: "ncov.png",
-    url: "/ncov/asia",
+    url: "/ncov/asia?f_region=Asia",
     title: "Latest analysis for Asia"
   },
   {
     img: "ncov.png",
-    url: "/ncov/oceania",
+    url: "/ncov/oceania?c=division&f_region=Oceania&r=division",
     title: "Latest analysis for Oceania"
   },
   {
     img: "ncov.png",
-    url: "/ncov/africa",
+    url: "/ncov/africa?f_region=Africa",
     title: "Latest analysis for Africa"
   },
   {
     img: "ncov.png",
-    url: "/ncov/south-america",
+    url: "/ncov/south-america?f_region=South%20America",
     title: "Latest analysis for South America"
   },
   {
