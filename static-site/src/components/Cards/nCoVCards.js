a9df7eff0e068f07cbf210188db8bdb1257f448e
cassiawag <41387591+cassiawag@users.noreply.github.com>
2020-08-14 11:36:31 -0600

    Update for newest sit-rep

--- static-site/src/components/Cards/nCoVCards.js
+++ static-site/src/components/Cards/nCoVCards.js
@@ -11,8 +11,8 @@ const nCoVCards = [
   },
   {
     img: "ncov_narrative.png",
-    url: "/narratives/ncov/sit-rep/2020-05-15",
-    title: "Latest Situation Report 2020-05-15"
+    url: "/narratives/ncov/sit-rep/2020-08-14",
+    title: "Latest Situation Report 2020-08-14"
   }
 ];
 
