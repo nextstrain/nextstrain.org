3b460708187250468a3fe4b2d2e8ccb75d1124d4
james hadfield <jh22@sanger.ac.uk>
2020-09-23 17:25:51 +1200

    Implement smoke-testing for API requests from the auspice client
    This commit implements basic smoke-testing for a variety of requests
    originiating from the Auspice client to the nextstrain.org server
    (i.e. API GET requests starting with `/charon/`).
    
    See the README for how to run

--- README.md
+++ README.md
@@ -166,6 +166,18 @@ It can be useful to test an unpublished auspice version -- modify `build.sh` to 
 
 
 ---
+## Testing
+Nextstrain.org currently implements limited smoke-testing to test requests originiating from the Auspice client to the nextstrain.org server (i.e. API GET requests starting with `/charon/`).
+These are defined [in this JSON file](./test/smoke-test/auspice_client_requests.json) and can be run via
+
+```
+npm run test:ci
+```
+
+Which will run a local server for the duration of the tests.
+Alternatively, you can run your own server in the background and run `npm run smoke-test`.
+
+---
 
 <p align="center">
   <img src="/docs/images/fred-hutch-logo-small.png" width="75" />
---
+++ test/smoke-test/auspice_client_requests.test.js
@@ -0,0 +1,45 @@
+
+const urlsToTest = require("./auspice_client_requests.json");
+const fetch = require('node-fetch');
+
+// As some of the APIs we're calling will themselves call  GitHub APIs
+// we set a lenient timeout
+jest.setTimeout(15000);
+
+describe("smoke testing URLs described in urls.json", () => {
+  for (const testCase of urlsToTest) {
+    // eslint-disable-next-line no-loop-func
+    it(testCase.name, async () => {
+      const url = `${BASE_URL}${testCase.url}`;
+      const res = await fetch(url, {redirect: 'manual'});
+
+      expect(res.status).toEqual(testCase.expectStatusCode || 200);
+
+      if (testCase.responseIsJson) await testResponseIsJson(res);
+
+      // If the test specifies a redirect, test that the header points to this
+      if (testCase.redirectsTo) testRedirect(res, `${BASE_URL}${testCase.redirectsTo}`);
+
+    });
+  }
+});
+
+async function testResponseIsJson(res) {
+  // to-do: can we use `expect` here? e.g. expect(async () => {await res.json();}).not.toThrow();
+  try {
+    await res.json();
+  } catch (e) {
+    throw Error(`Response wasn't JSON!`);
+  }
+}
+
+function testRedirect(res, expectedRedirectAddress) {
+  if (res.status!==302) {
+    throw Error(`Test asked to check redirect address, but statusCode wasn't 302`);
+  }
+  expect(getRedirectLocation(res)).toEqual(expectedRedirectAddress);
+}
+
+function getRedirectLocation(res) {
+  return res.headers[Object.getOwnPropertySymbols(res.headers)[0]].location[0];
+}
