2f2f9552fb365cb9ba570dc8470bbfd550a2cda5
eharkins <eli.harkins@gmail.com>
2021-04-15 17:35:29 -0700

    gatsby create page fix;
    this fixes unintended rendering of certain pages in the dir:
    static-site/src/pages. Gatsby auto-creates pages
    from js,jsx pages in this directory. This issue was raised here:
    https://github.com/nextstrain/nextstrain.org/pull/299#issuecomment-820696299
    In some cases, we want to call createPage in static-site/gatsby-node.js
    to create multiple other pages under a certain URL, so this
    puts those in static-site/src/sections as a workaround to avoid
    rendering of pages at a URL matching the exact name of the
    file (such as nextstrain.org/influenza-page). These
    files are intended to help render pages matching
    a specified URL (such as nextstrain.org/influenza/*).

--- static-site/gatsby-node.js
+++ static-site/gatsby-node.js
@@ -194,10 +194,14 @@ exports.createPages = ({graphql, actions}) => {
         });
 
         // Create page detailing all things SARS-CoV-2
+        // Note that this is in src/sections, not src/pages. This is because we don't
+        // want to render anything at the exact url of nextstrain.org/sars-cov-2-page
+        // and Gatsby auto-creates such pages from any jsx file in the src/pages directory.
+        // Many of the pages created here are in src/sections for the same reason.
         createPage({
           path: "/sars-cov-2",
           matchPath: "/sars-cov-2/*",
-          component: path.resolve("src/pages/sars-cov-2-page.jsx")
+          component: path.resolve("src/sections/sars-cov-2-page.jsx")
         });
 
         /* NOTE: we are using "influenza" URLs for dev purposes only. This will be switched to "flu"
@@ -209,20 +213,20 @@ exports.createPages = ({graphql, actions}) => {
         createPage({
           path: "/influenza",
           matchPath: "/influenza/*",
-          component: path.resolve("src/pages/influenza-page.jsx")
+          component: path.resolve("src/sections/influenza-page.jsx")
         });
 
         // search pages
         createPage({
           path: `/search`,
-          component: path.resolve("src/pages/sequence-search-list.jsx"),
+          component: path.resolve("src/sections/sequence-search-list.jsx"),
           context: {searchPages}
         });
         searchPages.forEach(({urlName, displayName, jsonUrl}) => {
           console.log(`created page at /search/${urlName}`);
           createPage({
             path: `/search/${urlName}`,
-            component: path.resolve("src/pages/sequence-search.jsx"),
+            component: path.resolve("src/sections/sequence-search.jsx"),
             context: {urlName, displayName, jsonUrl}
           });
         });
