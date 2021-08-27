424f327a7889577a99e4fcb981b6e5ceddba65a1
eharkins <eli.harkins@gmail.com>
2021-08-27 15:01:24 -0700

    Add flu cards to /influenza

---
+++ static-site/src/components/Cards/fluCards.js
@@ -0,0 +1,44 @@
+const fluCards = [
+  {
+    img: "seasonalinfluenza.png",
+    url: "/flu/seasonal/h3n2/ha/2y",
+    title: "A/H3N2"
+  },
+  {
+    img: "seasonalinfluenza.png",
+    url: "/flu/seasonal/h1n1pdm/ha/2y",
+    title: "A/H1N1pdm"
+  },
+  {
+    img: "seasonalinfluenza.png",
+    url: "/flu/seasonal/vic/ha/2y",
+    title: "B/Vic"
+  },
+  {
+    img: "seasonalinfluenza.png",
+    url: "/flu/seasonal/yam/ha/2y",
+    title: "B/Yam"
+  },
+  {
+    img: "avianinfluenza.png",
+    url: "/flu/avian/h5n1/ha",
+    title: "A/H5N1 (Avian)"
+  },
+  {
+    img: "avianinfluenza.png",
+    url: "/flu/avian/h5nx/ha",
+    title: "A/H5NX (Avian)"
+  },
+  {
+    img: "avianinfluenza.png",
+    url: "/flu/avian/h7n9/ha",
+    title: "A/H7N9 (Avian)"
+  },
+  {
+    img: "avianinfluenza.png",
+    url: "/flu/avian/h9n2/ha",
+    title: "A/H9N2 (Avian)"
+  }
+];
+
+export default fluCards;
