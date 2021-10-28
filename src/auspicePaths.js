f0c6dcde2edfb56507ed9c2d6c73f9c03772282b
Thomas Sibley <tsibley@fredhutch.org>
2021-10-28 10:39:50 -0700

    Remove the now-unused isRequestBackedByAuspiceDataset() function
    Sole remaining use was a unit test of the function itself.  Routing
    between Auspice and Gatsby is now declared via Express routers instead
    of implemented by this imperative function.  Tests to check the
    function's behaviour made sense, but IMO tests are less necessary with
    declarative routing rules.

--- src/auspicePaths.js
+++ src/auspicePaths.js
@@ -1,5 +1,4 @@
 const sources = require("./sources");
-const { splitPrefixIntoParts, parsePrefix } = require("./utils/prefix");
 
 /**
  * Auspice core builds have URLs where the first (`/`-separated) field matches the following list.
@@ -55,62 +54,7 @@ const potentialAuspiceRoutes = [
   "/fetch/*"
 ];
 
-/**
- * Express middleware to examine a path and determine if it should be routed to Auspice
- * for visualization, or sent a Gatsby-generated page.
- * This may require a fetch call for the main dataset (or the meta.json if v1 schema)
- * to decide if a URL is an auspice route or not.
- *
- * SIDE-EFFECTS:
- * Adds the following properties to `req`, which are used by subsequent middleware.
- * `req.sendToAuspice` {bool}
- */
-const isRequestBackedByAuspiceDataset = async (req, res, next) => {
-  // TODO: this is similar to `getDataset` and so we should refactor that
-  // and re-use it here so that we don't let them get out of sync.
-  req.sendToAuspice = false; // explicit default
-  try {
-
-    /* Previous to April 2021 work, _ALL_ these routes would be handled by Auspice.
-    We preserve previous behavior with this block, whilst slowly moving entries out
-    as they are build into gatsby.
-    (e.g. /groups/blab will not be handled by Auspice in the long run) */
-    const pathParts = req.path.replace(/^\//, '').replace(/\/$/, '').split("/");
-    if (
-      pathParts[0]==="status" ||
-      pathParts.includes("narratives") ||
-      pathParts[0]==="fetch"
-    ) {
-      req.sendToAuspice = true;
-      return next();
-    }
-
-    const {source, prefixParts} = splitPrefixIntoParts(req.path);
-    if (!source.visibleToUser(req.user)) {
-      return next();
-    }
-
-    /* For URLs which look like core builds we know that the start of the path must match a hardcoded list */
-    if (source.name==="core" && !coreBuilds.includes("/"+prefixParts[0])) {
-      return next();
-    }
-
-    /* Extract dataset information & check if it exists, storing the result as `req.sendToAuspice` */
-    /* (If a tangletree URL is requested, we only consider the first dataset here) */
-    const { dataset } = await parsePrefix(req.path.split(":")[0]);
-    if (await dataset.exists()) {
-      req.sendToAuspice = true;
-    }
-    return next();
-  } catch (err) {
-    console.log("isRequestBackedByAuspiceDataset", String(err));
-    req.sendToAuspice = false;
-    return next();
-  }
-};
-
 
 module.exports = {
-  isRequestBackedByAuspiceDataset,
   potentialAuspiceRoutes
 };
