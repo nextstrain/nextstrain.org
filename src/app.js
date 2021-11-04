0bab5f69e100cd035235cd1585beba34ac7dab72
Thomas Sibley <tsibley@fredhutch.org>
2021-11-04 12:49:51 -0700

    Clarify routes for narratives via /fetch
    The previous /fetch/:authority/* route worked for both datasets _and_
    narratives, despite using only setDataset() and never setNarrative(),
    because the source and dataset context objects are never actually used
    (e.g. the routes don't use ifDatasetExists or ifNarrativeExists).  Even
    if they were used, without other changes narratives would have been
    broken because the string "narratives" would have ended up as the source
    object's authority property.  Instead, Auspice's entrypoint was always
    served which resulted in the right client-side behaviour.
    
    Adjust the declared routes to be explicit about what we expect to handle
    and to parallel our routes for other sources, even if we still don't use
    the source, dataset, and narrative context objects.  This should also
    make future changes or additions to /fetch less surprising.

--- src/app.js
+++ src/app.js
@@ -176,8 +176,18 @@ app.routeAsync(["/community/:user/:repo", "/community/:user/:repo/*"])
 
 /* Datasets and narratives at ~arbitrary remote URLs.
  */
+app.use(["/fetch/narratives/:authority", "/fetch/:authority"],
+  setSource("fetch", req => req.params.authority));
+
+app.routeAsync("/fetch/narratives/:authority/*")
+  .all(setNarrative(req => req.params[0]))
+
+  // Assume existence; little benefit to checking when we don't have a fallback page.
+  .getAsync(sendAuspiceEntrypoint)
+;
+
 app.routeAsync("/fetch/:authority/*")
-  .all(setSource("fetch", req => req.params.authority), setDataset(req => req.params[0]))
+  .all(setDataset(req => req.params[0]))
 
   // Assume existence; little benefit to checking when we don't have a fallback page.
   .getAsync(sendAuspiceEntrypoint)
