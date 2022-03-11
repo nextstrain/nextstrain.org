17c11065fdf12ab8e18b51d14bfcf370fa127cb3
Jover <joverlee521@gmail.com>
2022-03-10 16:56:04 -0800

    FilterSelection: use dynamic row heights
    The filters can be a bit crowded on mobile due to the long strings,
    so this commit adds the same dynamic row height that is used in Auspice.

--- static-site/src/components/Datasets/filter-selection.jsx
+++ static-site/src/components/Datasets/filter-selection.jsx
@@ -2,7 +2,7 @@
  * Code and components related to Filtering.
  * Most code and logic was originally lifted from Auspice
  */
-import React from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import styled from 'styled-components';
 import { debounce, isEqual } from 'lodash';
 import ReactTooltip from 'react-tooltip';
@@ -11,11 +11,14 @@ import { createFilter } from "react-select";
 import AsyncSelect from "react-select/async";
 import { AutoSizer } from "react-virtualized/dist/es/AutoSizer";
 import { List } from "react-virtualized/dist/es/List";
+import { CellMeasurer, CellMeasurerCache } from "react-virtualized/dist/es/CellMeasurer";
 import 'react-virtualized/styles.css';
 import * as splashStyles from "../splash/styles";
 import { CenteredContainer } from "./styles";
 
 const DEBOUNCE_TIME = 200;
+const DEFAULT_ROW_HEIGHT = 40;
+const getOptionKeys = (options) => options.map((option) => option.key);
 
 const StyledTooltip = styled(ReactTooltip)`
   max-width: 30vh;
@@ -27,6 +30,15 @@ const StyledTooltip = styled(ReactTooltip)`
 `;
 
 const VirtualizedMenuList = ({ children, maxHeight, focusedOption }) => {
+  const listRef = useRef(null);
+  const optionKeys = useRef(null);
+  const cache = useRef(
+    new CellMeasurerCache({
+      fixedWidth: true,
+      defaultHeight: DEFAULT_ROW_HEIGHT
+    })
+  );
+
   /**
   * If the focused option is outside of the currently displayed options, we
   * need to create the scrollToIndex so that the List can be forced to scroll
@@ -42,16 +54,61 @@ const VirtualizedMenuList = ({ children, maxHeight, focusedOption }) => {
     }
   }
 
-  const rowRenderer = ({ index, key, style }) => (
-    <div key={key} style={style}>{children[index]}</div>
+  /**
+   * When the children options have changed, the cache has to be manually cleared
+   * and the list has to recompute the row heights to correctly display the
+   * new options.
+   * See answer from react-virtualized maintainer @bvaughn at
+   * https://stackoverflow.com/a/43837929
+   */
+  useLayoutEffect(() => {
+    if (children instanceof Array &&
+        !isEqual(optionKeys.current, getOptionKeys(children))) {
+      cache.current.clearAll();
+      listRef.current.recomputeRowHeights();
+      optionKeys.current = getOptionKeys(children);
+    }
+  }, [children]);
+
+  /**
+   * Wraps each option in a CellMeasurer which measures the row height based
+   * on the contents of the option and passes this height to the parent List
+   * component via the cache
+   */
+  const rowRenderer = ({ index, key, parent, style }) => (
+    <CellMeasurer
+      cache={cache.current}
+      columnIndex={0}
+      key={key}
+      parent={parent}
+      rowIndex={index}
+    >
+      <div style={style}>
+        {children[index]}
+      </div>
+    </CellMeasurer>
   );
+
+  /**
+   * Because the individual row heights are measured and cached on render,
+   * this is just a best guess of the list height.
+   * There can be a delay in the list height changing if there is a rapid
+   * change of the children options.
+   */
+  const calcListHeight = () => {
+    const currentRowHeights = Object.values(cache.current._rowHeightCache);
+    const totalRowHeight = currentRowHeights.reduce((a, b) => a + b, 0);
+    return totalRowHeight === 0 ? maxHeight : Math.min(maxHeight, totalRowHeight);
+  };
 
   return (
     <AutoSizer disableHeight >
       {({ width }) => (
         <List
-          height={maxHeight}
-          rowHeight={40}
+          ref={listRef}
+          height={calcListHeight()}
+          deferredMeasurementCache={cache.current}
+          rowHeight={cache.current.rowHeight}
           rowRenderer={rowRenderer}
           rowCount={children.length || 0}
           width={width}
