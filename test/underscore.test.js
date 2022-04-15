9c18442b59ed28df82b071a3e07287aca34ff2cc
skorman <skorman@umich.edu>
2022-04-15 14:18:52 -0400

    Refactored underscore test suite

--- test/underscore.test.js
+++ test/underscore.test.js
@@ -10,17 +10,13 @@ const fs = require("fs");
 
 const gatsby404 = fs.readFileSync("static-site/public/404.html").toString();
 
-/* It would be better if none of these tests relied on actual production
- * datasets/narratives and instead solely used fixtures explicitly put in place
- * for testing (like the nextstrain/community-test repo).  However, it is not
- * just a matter of uploading fixtures to our various sources, as then they'll
- * automatically show up in listings of dataset, narratives, groups, etc.  We
- * don't currently have a way to mark things as "hidden", and adding a way is
- * not a tangent I want to embark on right now.
- *
- * Until then, this test file may be a bit more fragile than is ideal, but it
- * should still be better than not testing.
- *   -trs, 23 Nov 2021
+/* Tests for issue #432, where we would like to reject dataset paths that contain
+ * underscores. This test set could be merged into requests.test.js but I
+ * kept it separate for now because many of those tests need to be run on the
+ * server or require the AWS credentials. These tests only use the public
+ * flu dataset (which is why it is a small test suite), but should be able
+ * to be run by any developer.
+ *   -sk, 15 Apr 2022
  */
 
 /* Set a lenient timeout of 15s as these depend on remote/upstream servers
@@ -34,8 +30,7 @@ describe(`test underscores`, () => {
   const cases = [
     {case: "error", path: "/groups/blab/ncov_early-outbreak_root-A", expected: 404},
     {case: "error", path: "/flu/seasonal/h3n2_ha_2y", expected: 404},
-    {case: "success", path: "/groups/blab/ncov-early-outbreak-root-A", expected: 200},
-    {case: "success", path: "/flu/seasonal/h3n2/ha/2yA", expected: 200}
+    {case: "success", path: "/flu/seasonal/h3n2/ha/2y", expected: 200}
   ];
 
   cases.forEach(testPath);
@@ -45,17 +40,15 @@ describe(`test underscores`, () => {
       const req = fetch(url(path), accept("text/html,*/*;q=0.1"));
 
 
-      test(`TESTING UNDERSCORE ${case_}`, async () => {
+      test(`TESTING UNDERSCORE ${path}`, async () => {
         const res = await req;
         expect(res.status).toBe(expected);
       });
 
-      test(`TESTING BODY ${case_}`, async () => {
+      test(`TESTING BODY ${path}`, async () => {
         const res = await req;
         const body = await res.text();
-        if (expected == 404) {
-          expect(body).toBe(gatsby404);
-        } else {
+        if (expected != 404) {
           expect(body).not.toBe(gatsby404);
         }
       });
