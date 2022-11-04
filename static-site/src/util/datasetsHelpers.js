ca90de74768de8d55280e9d251c71173337ebc68
Thomas Sibley <tsibley@fredhutch.org>
2022-11-04 13:42:11 -0700

    Check HTTP response status during client-side JSON fetches
    This avoids silent failures and propagation of unexpected data.
    
    Throwing an error if the status isn't ok is better than blindly
    deserializing the response as JSON and assuming that error responses
    won't deserialize.  Particularly, that assumption fails when the error
    response *itself* is JSON, as we sometimes produce.
    
    Related-to: <https://github.com/nextstrain/nextstrain.org/issues/617>

--- static-site/src/util/datasetsHelpers.js
+++ static-site/src/util/datasetsHelpers.js
@@ -1,5 +1,7 @@
 
 export const fetchAndParseJSON = async (jsonUrl) => {
-  return fetch(jsonUrl, { headers: { Accept: 'application/json' }})
-    .then((res) => res.json());
+  return fetch(jsonUrl, { headers: { Accept: 'application/json' }}).then((res) => {
+    if (!res.ok) throw new Error(`fetch failed: ${res.status} ${res.statusText}`);
+    return res.json();
+  });
 };
