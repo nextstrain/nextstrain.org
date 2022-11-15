57cd68a38f66cfe20cafbfb49c8c09fe572f117d
Thomas Sibley <tsibley@fredhutch.org>
2022-11-15 10:49:01 -0800

    team: Make all links https://

--- static-site/src/pages/team.jsx
+++ static-site/src/pages/team.jsx
@@ -11,14 +11,14 @@ import { CenteredFocusParagraph } from "../components/splash/styles";
  */
 
 export const teamMembers = [
-  {name: "Trevor Bedford", image: "trevor-bedford.jpg", link: "http://bedford.io/team/trevor-bedford/"},
+  {name: "Trevor Bedford", image: "trevor-bedford.jpg", link: "https://bedford.io/team/trevor-bedford/"},
   {name: "Richard Neher", image: "richard-neher.jpg", link: "https://neherlab.org/richard-neher.html"},
-  {name: "James Hadfield", image: "james-hadfield.jpg", link: "http://bedford.io/team/james-hadfield/"},
-  {name: "Emma Hodcroft", image: "emma-hodcroft.jpg", link: "http://emmahodcroft.com/"},
+  {name: "James Hadfield", image: "james-hadfield.jpg", link: "https://bedford.io/team/james-hadfield/"},
+  {name: "Emma Hodcroft", image: "emma-hodcroft.jpg", link: "https://emmahodcroft.com/"},
   {name: "Thomas Sibley", image: "thomas-sibley.jpg", link: "https://bedford.io/team/thomas-sibley/"},
-  {name: "John Huddleston", image: "john-huddleston.jpg", link: "http://bedford.io/team/john-huddleston/"},
+  {name: "John Huddleston", image: "john-huddleston.jpg", link: "https://bedford.io/team/john-huddleston/"},
   {name: "Ivan Aksamentov", image: "ivan-aksamentov.jpg", link: "https://neherlab.org/ivan-aksamentov.html"},
-  {name: "Jover Lee", image: "jover-lee.jpg", link: "http://bedford.io/team/jover-lee/"},
+  {name: "Jover Lee", image: "jover-lee.jpg", link: "https://bedford.io/team/jover-lee/"},
   {name: "Jennifer Chang", image: "jennifer-chang.jpg", link: "https://bedford.io/team/jennifer-chang/"},
   {name: "Victor Lin", image: "victor-lin.png", link: "https://bedford.io/team/victor-lin/"},
   {name: "Cornelius Roemer", image: "cornelius-roemer.jpg", link: "https://neherlab.org/cornelius-roemer.html"},
@@ -27,17 +27,17 @@ export const teamMembers = [
 const alumni = [
   {name: "Cassia Wagner", image: "cassia-wagner.jpg", link: "https://bedford.io/team/cassia-wagner/"},
   {name: "Miguel Parades", image: "miguel-parades.jpg", link: "https://bedford.io/team/miguel-parades/"},
-  {name: "Kairsten Fay", image: "kairsten-fay.jpg", link: "http://bedford.io/team/kairsten-fay/"},
+  {name: "Kairsten Fay", image: "kairsten-fay.jpg", link: "https://bedford.io/team/kairsten-fay/"},
   {name: "Moira Zuber", image: "moira-zuber.jpg"},
   {name: "Eli Harkins", image: "eli-harkins.jpg", link: "https://bedford.io/team/eli-harkins/"},
-  {name: "Misja Ilcisin", image: "misja-ilcisin.jpg", link: "http://bedford.io/team/misja-ilcisin/"},
-  {name: "Louise Moncla", image: "louise-moncla.jpg", link: "http://bedford.io/team/louise-moncla/"},
-  {name: "Allison Black", image: "allison-black.jpg", link: "http://bedford.io/team/allison-black/"},
-  {name: "Sidney Bell", image: "sidney-bell.jpg", link: "http://bedford.io/team/sidney-bell/"},
-  {name: "Colin Megill", image: "colin-megill.jpg", link: "http://www.colinmegill.com/"},
-  {name: "Barney Potter", image: "barney-potter.jpg", link: "http://bedford.io/team/barney-potter/"},
+  {name: "Misja Ilcisin", image: "misja-ilcisin.jpg", link: "https://bedford.io/team/misja-ilcisin/"},
+  {name: "Louise Moncla", image: "louise-moncla.jpg", link: "https://bedford.io/team/louise-moncla/"},
+  {name: "Allison Black", image: "allison-black.jpg", link: "https://bedford.io/team/allison-black/"},
+  {name: "Sidney Bell", image: "sidney-bell.jpg", link: "https://bedford.io/team/sidney-bell/"},
+  {name: "Colin Megill", image: "colin-megill.jpg", link: "https://www.colinmegill.com/"},
+  {name: "Barney Potter", image: "barney-potter.jpg", link: "https://bedford.io/team/barney-potter/"},
   {name: "Pavel Sagulenko", image: "pavel-sagulenko.jpg", link: "https://neherlab.org/pavel-sagulenko.html"},
-  {name: "Charlton Callender", image: "charlton-callender.jpg", link: "http://bedford.io/team/charlton-callender/"},
+  {name: "Charlton Callender", image: "charlton-callender.jpg", link: "https://bedford.io/team/charlton-callender/"},
 ]
 
 const Grid = styled.div`
