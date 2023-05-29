a75539ac59d4441024d97b77b2798ec43425b34b
James Hadfield <hadfield.james@gmail.com>
2023-05-29 15:15:15 +1200

    Remove dataset listing /influenza, /staging
    The JSON backing the influenza listing was empty, and the JSON backing
    the staging dataset was extremely incomplete (29 datasets total).
    I presume this is due to the truncated results of listing the bucket.
    Rather than attempt to fix the script, I think it's advisable to revisit
    these concepts and what we really want to do here. While this is
    happening it's better to remove the incomplete list of datasets.
    
    These pages were implemented with the hope

--- static-site/src/sections/influenza-page.jsx
+++ static-site/src/sections/influenza-page.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ScrollableAnchor, { configureAnchors } from "react-scrollable-anchor";
 import {
   SmallSpacer,
   MediumSpacer,
@@ -8,14 +7,10 @@ import {
 } from "../layouts/generalComponents";
 import * as splashStyles from "../components/splash/styles";
 import { PathogenPageIntroduction } from "../components/Datasets/pathogen-page-introduction";
-import DatasetSelect from "../components/Datasets/dataset-select";
 import GenericPage from "../layouts/generic-page";
-import { fetchAndParseJSON } from "../util/datasetsHelpers";
-import { DataFetchErrorParagraph, ErrorBanner } from "../components/splash/errorMessages";
+import { ErrorBanner } from "../components/splash/errorMessages";
 import Cards from "../components/Cards/index";
 import fluCards from "../components/Cards/fluCards";
-
-const nextstrainLogoPNG = "/favicon.png";
 
 const title = "Influenza resources";
 const abstract = `The Nextstrain team maintains datasets and other tools for analyzing a variety of influenza viruses.
@@ -26,11 +21,6 @@ and domestic birds, including novel reassortant H5 viruses.`;
 
 const contents = [
   {
-    type: "anchor",
-    to: "datasets",
-    title: "Scroll down to all available datasets"
-  },
-  {
     type: "gatsby",
     to: "/search/seasonal-flu",
     title: "Search seasonal flu datasets by strain name(s)",
@@ -38,56 +28,14 @@ const contents = [
   }
 ];
 
-const tableColumns = [
-  {
-    name: "Dataset",
-    value: (dataset) => dataset.filename.replace(/_/g, ' / ').replace('.json', ''),
-    url: (dataset) => dataset.url
-  },
-  {
-    name: "Contributor",
-    value: () => "Nextstrain",
-    valueMobile: () => "",
-    url: () => "https://nextstrain.org",
-    logo: () => (<img alt="nextstrain.org" className="logo" width="24px" src={nextstrainLogoPNG}/>)
-  },
-  {
-    name: "Uploaded Date",
-    value: (dataset) => dataset.date_uploaded
-  }
-];
-
-
 class Index extends React.Component {
   constructor(props) {
     super(props);
-    configureAnchors({ offset: -10 });
-    this.state = {
-      dataLoaded: false,
-      errorFetchingData: false,
-      // scripts/collect-datasets.js collects datasets
-      // from s3 and writes them to a JSON which is pushed
-      // regularly to s3 as a resource we request here
-      // representing a list of datasets to display on
-      // this page with some info about each.
-      datasetsUrl: "https://data.nextstrain.org/datasets_influenza.json"
-    };
+    this.state = {}
   }
+
   async componentDidMount() {
-    let datasets;
-    let dataLoaded, errorFetchingData = false;
-    try {
-      datasets = await fetchAndParseJSON(this.state.datasetsUrl);
-      dataLoaded = true;
-    } catch (err) {
-      console.error("Error fetching / parsing data.", err.message);
-      errorFetchingData = true;
-    }
     this.setState({
-      datasets,
-      dataLoaded,
-      errorFetchingData,
-      // For some reason if this is set in the constructor it breaks the banner.
       nonExistentDatasetName: this.props["*"]
     });
   }
@@ -108,7 +56,7 @@ class Index extends React.Component {
         <splashStyles.H1>{title}</splashStyles.H1>
         <SmallSpacer />
 
-        <FlexCenter>
+        <FlexCenter>  
           <splashStyles.CenteredFocusParagraph>
             {abstract}
           </splashStyles.CenteredFocusParagraph>
@@ -119,23 +67,7 @@ class Index extends React.Component {
 
         <MediumSpacer/>
         <Cards squashed cards={fluCards}/>
-
-        <ScrollableAnchor id={"datasets"}>
-          <div>
-            <HugeSpacer /><HugeSpacer />
-            <splashStyles.H2>
-              Influenza datasets
-            </splashStyles.H2>
-            <MediumSpacer />
-            {this.state.dataLoaded && (
-              <DatasetSelect
-                datasets={this.state.datasets}
-                columns={tableColumns}
-              />
-            )}
-            { this.state.errorFetchingData && <DataFetchErrorParagraph /> }
-          </div>
-        </ScrollableAnchor>
+        <HugeSpacer/>
       </GenericPage>
     );
   }
