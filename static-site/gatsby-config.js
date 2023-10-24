e98aeaa41536ec7fe12e311de4ede1ef99124438
Victor Lin <13424970+victorlin@users.noreply.github.com>
2023-10-24 15:51:03 -0700

    Remove unused config

--- static-site/gatsby-config.js
+++ static-site/gatsby-config.js
@@ -1,21 +1,7 @@
 const config = require("./data/SiteConfig");
 
-// const pathPrefix = config.pathPrefix === "/" ? "" : config.pathPrefix;
-
 module.exports = {
   pathPrefix: config.pathPrefix,
-  // siteMetadata: {
-  //   siteUrl: config.siteUrl + pathPrefix,
-  //   rssMetadata: {
-  //     site_url: config.siteUrl + pathPrefix,
-  //     feed_url: config.siteUrl + pathPrefix + config.siteRss,
-  //     title: config.siteTitle,
-  //     description: config.siteDescription,
-  //     image_url: `${config.siteUrl + pathPrefix}/logos/logo-512.png`,
-  //     author: config.userName,
-  //     copyright: config.copyright
-  //   }
-  // },
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-styled-components",
