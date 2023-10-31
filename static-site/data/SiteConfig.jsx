8cf1b53dadfc9935d0c89f521342279d598d8685
Victor Lin <13424970+victorlin@users.noreply.github.com>
2023-10-31 11:48:30 -0700

    3.2: Customize Groups components

--- static-site/data/SiteConfig.jsx
+++ static-site/data/SiteConfig.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { Link } from "gatsby";
-import NextstrainFooter from "../src/components/Footer";
+import { IconParagraph } from "../src/components/splash/styles";
 
-export const siteTitle = "Nextstrain";
+export const siteTitle = "Nextstrain @ Contoso";
 
 // Alternative site title for SEO.
 export const siteTitleAlt = "Real-time tracking of pathogen evolution";
@@ -17,37 +16,35 @@ export const siteDescription = "Real-time tracking of pathogen evolution";
 
 export const groupsApp = true;
 
-export const groupsTitle = "Scalable Sharing with Nextstrain Groups";
+export const groupsTitle = "Contoso Nextstrain Groups Server";
 
 export const ErrorBannerInitialMessage = () => (<>
-  Please <a href="mailto:hello@nextstrain.org">contact us at hello@nextstrain.org</a> if you believe this to be an error.
+  Please <a href="mailto:support@contoso.com">contact us at support@contoso.com</a> if you believe this to be an error.
 </>);
 
 export const DataFetchError = () => (<>
   Something went wrong getting data.
-  Please <a href="mailto:hello@nextstrain.org">contact us at hello@nextstrain.org</a> if this continues to happen.
+  Please <a href="mailto:support@contoso.com">contact us at support@contoso.com</a> if this continues to happen.
 </>);
 
 export const GroupsAbstract = () => (<>
-  We want to enable research labs, public health entities
-  and others to share their datasets and narratives through Nextstrain with
-  complete control of their data and audience. Nextstrain Groups is more scalable
-  than community builds in both data storage and viewing permissions. Datasets in
-  a public group are accessible to the general public via nextstrain.org, while
-  private group data are only visible to logged in users with permission to see
-  the data. A single entity can manage both a public and a private group in order
-  to share data with different audiences.
-  <br />
-  <br />
-  For more details about Nextstrain Groups,
-  <a href="https://docs.nextstrain.org/en/latest/learn/groups/index.html"> please see our documentation</a>.
-  For an alternative approach to sharing data through nextstrain.org which leverages GitHub repositories, please see
-  <Link to="/community"> Community data sharing</Link>.
-  <br />
-  <br />
-  Nextstrain Groups is still in the early stages and require a Nextstrain team
-  member to set up and add users.
-  Please <a href="mailto:hello@nextstrain.org">contact us at hello@nextstrain.org</a> and we’d be happy to set up a group for you.
+    This is Contoso Corporation's internal Nextstrain Groups Server.
+    <br />
+    <br />
+    For more details about Nextstrain Groups,
+    <a href="https://docs.contoso.com/nextstrain-groups"> please see our documentation</a>.
+    <br />
+    <br />
+    Nextstrain Groups is still in the early stages and require a service administrator to set up and add users.
+    Please <a href="mailto:support@contoso.com">contact us at support@contoso.com</a> and we’d be happy to set up a group for you.
 </>);
 
-export const Footer = NextstrainFooter;
+export const Footer = () => (<>
+  <IconParagraph>
+    {"Hadfield "}<i>{"et al., "}</i>
+    <a href="https://doi.org/10.1093/bioinformatics/bty407" target="_blank" rel="noreferrer noopener">Nextstrain: real-time tracking of pathogen evolution</a>
+    <i>, Bioinformatics</i> (2018)
+    <div style={{ margin: "10px 0px" }} />
+    Developed by the Nextstrain team and hosted by Contoso.
+  </IconParagraph>
+</>);
