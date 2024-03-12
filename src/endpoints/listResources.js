8c65887f96425f0a6301a9f39ada44dc99567814
james hadfield <hadfield.james@gmail.com>
2024-03-12 17:29:35 +1300

    Add /list-resources JSON-only API
    The current route and data format should be considered "beta" and there
    is a loud warning message included in the JSON response indicating this.
    The current implementation only considers a single source and only for
    "dataset" resources, as that's the (short-term) expected usage in the
    UI.

---
+++ src/endpoints/listResources.js
@@ -0,0 +1,28 @@
+import {ListResources} from "../resourceIndex.js";
+import * as utils from '../utils/index.js';
+import { contentTypesProvided } from '../negotiate.js';
+
+/**
+ * An express route handler to send data (or throw an error) for the requested
+ * resources
+ * 
+ * @type {asyncExpressHandler}
+ */
+const listResourcesJson = async (req, res) => {
+  /* API currently only handles a limited range of sources / resource types.
+  ListResources will throw a HTTP error if they do not exist */
+  const resourceType = 'dataset';
+  const sourceName = req.params.sourceName;
+  const resources = new ListResources([sourceName], [resourceType]);
+  const data = {
+    WARNING: `This API is intended for internal use only. The API, including the address, may change at any point without notice.`,
+    ...resources.data
+  }
+  utils.verbose(`[LIST RESOURCES] Sending JSON for source "${sourceName}" resource type "${resourceType}"`);
+  return res.json(data);
+};
+
+export const listResources = contentTypesProvided([
+  ["text/html", (req, res, next) => next()], /* go to to next acceptable routing middleware (currently Gatsby) */
+  ["application/json", listResourcesJson],
+])
