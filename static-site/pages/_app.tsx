c073f1e4a7eea98e950202d7e799f03c9b2880b6
james hadfield <hadfield.james@gmail.com>
2024-04-08 12:24:59 +1200

    Enable plausible analytics
    I investigated the plausible-recommended plugin¹ but for our usage we
    only need a single <script> tag across the site. Tested on localhost by
    using `script.local.js`²
    
    We should consider proxying the script though our site as a proportion
    (perhaps as high as 60%) of people will block this script, but that's
    left as future work.
    
    ¹ <https://github.com/4lejandrito/next-plausible>
    ² <https://plausible.io/docs/script-extensions>
    ³ <https://plausible.io/docs/proxy/introduction>

--- static-site/pages/_app.tsx
+++ static-site/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import { Lato } from 'next/font/google'
+import Script from 'next/script'
 
 import "../src/styles/browserCompatability.css";
 import "../src/styles/bootstrap.css";
@@ -16,6 +17,21 @@ const lato = Lato({
 
 export default function App({ Component, pageProps }: AppProps) {
   return <div className={lato.variable}>
+    <PlausibleAnalytics/>
     <Component {...pageProps} />
   </div>
+}
+
+function PlausibleAnalytics() {
+  /* See <https://plausible.io/docs/plausible-script>. Analytics are disabled on
+     localhost, but use `script.local.js` if you want to track localhost for
+     testing purposes */
+  return (
+    <Script
+      src="https://plausible.io/js/script.js" 
+      data-domain="nextstrain.org"
+      async
+      defer
+    />
+  )
 }
