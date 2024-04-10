6e97d3cc3cf36aa3362c8629edc88b0957bfecba
james hadfield <hadfield.james@gmail.com>
2024-04-10 13:21:14 +1200

    Helpful error when static-site needs to be built
    The error message Next.js prints isn't obvious if you're not familiar
    with the various moving parts so this error message should help.
    
    This was prompted by review comment <https://github.com/nextstrain/nextstrain.org/pull/810#discussion_r1558304436>

--- src/endpoints/nextjs.js
+++ src/endpoints/nextjs.js
@@ -1,11 +1,17 @@
 import next from 'next';
 import { STATIC_SITE_PRODUCTION } from '../config.js';
-import {verbose} from '../utils/index.js';
+import {verbose, error} from '../utils/index.js';
 
 const PORT = Number(process.env.PORT || 5000);
 
 const nextJsApp = next({dev: !STATIC_SITE_PRODUCTION, port: PORT, dir: './static-site'})
-const prepared = nextJsApp.prepare();
+const prepared = nextJsApp.prepare()
+  .catch((err) => {
+    if (STATIC_SITE_PRODUCTION && String(err).startsWith('Error: ENOENT: no such file or directory')) {
+      error(`To run the next.js site in production mode it needs to be built via 'npm run build' or 'npx next build static-site'`);
+    }
+    throw err; // will print stack trace & is fatal for the server
+  })
 const _handleRequest = nextJsApp.getRequestHandler();
 
 export async function handleRequest(req, res) {
