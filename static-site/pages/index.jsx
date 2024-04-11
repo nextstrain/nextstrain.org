81062166712258c99e5ea7dfd9ff4ea8a2fd7c0c
james hadfield <hadfield.james@gmail.com>
2024-04-11 12:46:31 +1200

    WIP enable ssr for /pathogens and /
    the unhydrated site looks pretty good but there are hydration errors
    raised by next.js (only in dev mode)

--- static-site/pages/index.jsx
+++ static-site/pages/index.jsx
@@ -1,3 +1,8 @@
+/**
+ * 
+ * Example of  dynamic import but using ssr (which is the default dynamic import!)
+ */
+
 import dynamic from 'next/dynamic'
-const Index = dynamic(() => import("../src/pages/index"), {ssr: false})
+const Index = dynamic(() => import("../src/pages/index"), {ssr: true})
 export default Index;
--- static-site/pages/pathogens.jsx
+++ static-site/pages/pathogens.jsx
@@ -1,3 +1,8 @@
+/**
+ * 
+ * Example of  dynamic import but using ssr (which is the default dynamic import!)
+ */
+
 import dynamic from 'next/dynamic'
-const Index = dynamic(() => import("../src/sections/pathogens"), {ssr: false})
+const Index = dynamic(() => import("../src/sections/pathogens"), {ssr: true})
 export default Index;
