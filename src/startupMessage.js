3a9d2af2b19e4dfa0e93b062417c14d0a358d0f5
james hadfield <hadfield.james@gmail.com>
2024-04-12 09:55:13 +1200

    [startup message] update server self-description
    The server is now often _not_ running at nextstrain.org; the core team
    runs additional dev + canary servers, we have Heroku review apps as well
    as external groups who run our server. (And I presume most of the people
    reading the startup message are actually running locally.)

--- src/startupMessage.js
+++ src/startupMessage.js
@@ -3,16 +3,14 @@
 export const nextstrainAbout = `
   Nextstrain is an open-source project to harness the scientific and public
   health potential of pathogen genome data.
-
-  This is the server behind nextstrain.org.
   See https://github.com/nextstrain/nextstrain.org for more.
 `;
 
 export async function startupMessage(server, app) {
   console.log("  -------------------------------------------------------------------------");
+  console.log('                             NEXTSTRAIN SERVER');
   console.log(nextstrainAbout);
-  console.log(`  Server listening on port ${server.address().port}`);
-  console.log(`  Accessible at https://nextstrain.org or http://localhost:${server.address().port}`);
+  console.log(`  Server listening on port ${server.address().port}, accessible at http://localhost:${server.address().port}`);
   console.log(`  Server is running in ${app.locals.production ? 'production' : 'development'} mode`);
   if (app.locals.STATIC_SITE_PRODUCTION) {
     console.log(`  Next.js frontend is running in production mode (statically generated assets)`)
