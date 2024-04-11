cf1afcbe0b897caebcc0abba0d206a79cdc2f4d5
james hadfield <hadfield.james@gmail.com>
2024-04-12 09:48:50 +1200

    [startup message] refactor
    In preparation for upcoming additions. The additions will require async
    calls so we add a simple log statement at the earliest time the server
    is actually listening which may be helpful for certain debugging
    situations.

---
+++ src/startupMessage.js
@@ -0,0 +1,23 @@
+
+
+export const nextstrainAbout = `
+  Nextstrain is an open-source project to harness the scientific and public
+  health potential of pathogen genome data.
+
+  This is the server behind nextstrain.org.
+  See https://github.com/nextstrain/nextstrain.org for more.
+`;
+
+export async function startupMessage(server, app) {
+  console.log("  -------------------------------------------------------------------------");
+  console.log(nextstrainAbout);
+  console.log(`  Server listening on port ${server.address().port}`);
+  console.log(`  Accessible at https://nextstrain.org or http://localhost:${server.address().port}`);
+  console.log(`  Server is running in ${app.locals.production ? 'production' : 'development'} mode`);
+  if (app.locals.STATIC_SITE_PRODUCTION) {
+    console.log(`  Next.js frontend is running in production mode (statically generated assets)`)
+  } else {
+    console.log(`  Next.js frontend is running in development mode (hot reloading etc)`)
+  }
+  console.log("\n  -------------------------------------------------------------------------\n\n");
+}
