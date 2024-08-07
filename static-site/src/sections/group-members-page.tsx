9ac085dc1a298972294f85af7973dc10b03c0ae5
Jover Lee <joverlee521@gmail.com>
2024-08-06 17:30:08 -0700

    wip

--- static-site/src/sections/group-members-page.tsx
+++ static-site/src/sections/group-members-page.tsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { FaTrash } from "react-icons/fa";
 import { uri } from "../../../src/templateLiterals.js";
 import GenericPage from "../layouts/generic-page.jsx";
 import { BigSpacer, CenteredContainer, FlexGridRight, MediumSpacer } from "../layouts/generalComponents.jsx";
 import * as splashStyles from "../components/splash/styles";
 import { ErrorBanner } from "../components/errorMessages.jsx";
+import { InputButton } from "../components/Groups/styles.jsx";
 
 interface GroupMember {
   username: string,
@@ -16,52 +18,59 @@ interface ErrorMessage {
 }
 
 const GroupMembersPage = ({ groupName }: {groupName: string}) => {
-  const [ canEditMembers, setCanEditMembers ] = useState<boolean>(false);
   const [ errorMessage, setErrorMessage ] = useState<ErrorMessage>({title: "", contents: ""});
   const [ roles, setRoles ] = useState<string[]>([]);
   const [ members, setMembers ] = useState<GroupMember[]>([]);
+  const [ canEditMembers, setCanEditMembers ] = useState<boolean>(false);
 
   useEffect(() => {
-    async function getGroupMembership(groupName: string) {
-      const headers = { headers: {"Accept": "application/json"}};
-      const canEditMembers = await canEditGroupMembers(groupName);
-      let roles, members = [];
-      try {
-        const [ rolesResponse, membersResponse ] = await Promise.all([
-          fetch(uri`/groups/${groupName}/settings/roles`, headers),
-          fetch(uri`/groups/${groupName}/settings/members`, headers)
-        ]);
-        if (!rolesResponse.ok) {
-          throw new Error(`Fetching group roles failed: ${rolesResponse.status} ${rolesResponse.statusText}`)
-        }
-        if (!membersResponse.ok) {
-          throw new Error(`Fetching group members failed: ${membersResponse.status} ${membersResponse.statusText}`)
-        }
-        roles = await rolesResponse.json();
-        members = await membersResponse.json();
-      } catch (err) {
-        const errorMessage = (err as Error).message
-        if(!ignore) {
-          setErrorMessage({
-            title: "An error occurred when trying to fetch group membership data",
-            contents: errorMessage})
-        }
-      }
-      return {canEditMembers, roles, members};
-    };
-
     let ignore = false;
     getGroupMembership(groupName).then(result => {
       if (!ignore) {
-        setCanEditMembers(result.canEditMembers);
         setRoles(result.roles);
         setMembers(result.members);
+        setCanEditMembers(result.canEditMembers);
       }
     })
     return () => {
       ignore = true;
     };
   }, [groupName]);
+
+  async function getGroupMembership(groupName: string) {
+    const headers = { headers: {"Accept": "application/json"}};
+    const canEditMembers = await canEditGroupMembers(groupName);
+    let roles, members = [];
+    try {
+      const [ rolesResponse, membersResponse ] = await Promise.all([
+        fetch(uri`/groups/${groupName}/settings/roles`, headers),
+        fetch(uri`/groups/${groupName}/settings/members`, headers)
+      ]);
+      if (!rolesResponse.ok) {
+        throw new Error(`Fetching group roles failed: ${rolesResponse.status} ${rolesResponse.statusText}`)
+      }
+      if (!membersResponse.ok) {
+        throw new Error(`Fetching group members failed: ${membersResponse.status} ${membersResponse.statusText}`)
+      }
+      roles = await rolesResponse.json();
+      members = await membersResponse.json();
+    } catch (err) {
+      const errorMessage = (err as Error).message
+      setErrorMessage({
+        title: "An error occurred when trying to fetch group membership data",
+        contents: errorMessage})
+    }
+    return {canEditMembers, roles, members};
+  };
+
+  async function removeMember(member: GroupMember) {
+    const responses = await Promise.all(
+      member.roles.map((role) => fetch(uri`/groups/${groupName}/settings/roles/${role}/members/${member.username}`, {method: "DELETE"}))
+    );
+    if (responses.every((response) => response.ok)) {
+
+    }
+  }
 
   return (
     <GenericPage banner={errorMessage.title ? <ErrorBanner {...errorMessage} /> : undefined}>
@@ -78,13 +87,24 @@ const GroupMembersPage = ({ groupName }: {groupName: string}) => {
       <BigSpacer/>
 
       {roles && members
-        ? <MembersTable roles={roles as string[]} members={members as GroupMember[]} />
+        ? <MembersTable
+            roles={roles as string[]}
+            members={members as GroupMember[]}
+            canEditMembers={canEditMembers}
+            removeMember={removeMember}
+          />
         : <splashStyles.H4>Fetching group members...</splashStyles.H4>}
     </GenericPage>
   )
 };
 
-const MembersTable = ({ roles, members }: { roles: string[], members: GroupMember[]}) => {
+const MembersTable = ({ roles, members, canEditMembers, removeMember }:
+  {
+    roles: string[],
+    members: GroupMember[],
+    canEditMembers: boolean,
+    removeMember: (member: GroupMember) => void
+  }) => {
   const sortedMembers = members.toSorted((a, b) => a.username.localeCompare(b.username));
   function mostPrivilegedRole(memberRoles: string[]) {
     // Assumes that the provided roles are listed in order of least to most privileged
@@ -94,30 +114,42 @@ const MembersTable = ({ roles, members }: { roles: string[], members: GroupMembe
   return (
     <CenteredContainer>
       <div className="row">
-        <div className="col-6">
+        <div className="col">
           <splashStyles.CenteredFocusParagraph>
             <strong>Username</strong>
           </splashStyles.CenteredFocusParagraph>
         </div>
-        <div className="col-6">
+        <div className="col">
           <splashStyles.CenteredFocusParagraph>
             <strong>Roles</strong>
           </splashStyles.CenteredFocusParagraph>
         </div>
+        {canEditMembers &&
+          <div className="col">
+            <splashStyles.CenteredFocusParagraph>
+              <strong>Remove Member</strong>
+            </splashStyles.CenteredFocusParagraph>
+          </div>}
       </div>
 
     {sortedMembers.map((member) =>
       <div className="row" key={member.username}>
-        <div className="col-6">
+        <div className="col">
           <splashStyles.CenteredFocusParagraph>
             {member.username}
           </splashStyles.CenteredFocusParagraph>
         </div>
-        <div className="col-6">
+        <div className="col">
           <splashStyles.CenteredFocusParagraph>
             {mostPrivilegedRole(member.roles)}
           </splashStyles.CenteredFocusParagraph>
         </div>
+        {canEditMembers &&
+          <div className="col d-flex align-items-center justify-content-center">
+            <InputButton onClick={() => removeMember(member)}>
+              <FaTrash/>
+            </InputButton>
+          </div>}
       </div>
       )}
     </CenteredContainer>
