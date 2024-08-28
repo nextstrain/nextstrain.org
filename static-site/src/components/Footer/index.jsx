cc3d4dff917376bda052fb95888e8150cd07f277
Jover Lee <joverlee521@gmail.com>
2024-08-28 15:45:25 -0700

    static-site: Convert Footer to functional component
    Doing this in preparation for changing the pathname check to using
    `usePathname`, which is a hook that only works in functional components.

--- static-site/src/components/Footer/index.jsx
+++ static-site/src/components/Footer/index.jsx
@@ -5,80 +5,78 @@ import { Logos } from "../../components/logos";
 import { FooterList } from "../People/avatars";
 import { SiteMap } from "./sitemap";
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div>
-        <SiteMap />
+const Footer = () => {
+  return (
+    <div>
+      <SiteMap />
 
-        <BigSpacer />
+      <BigSpacer />
 
-        <div className="row">
-          <div className="col-lg-1"/>
-          <div className="col-lg-10">
-            <Styles.IconParagraph>
-              {"Hadfield "}<i>{"et al., "}</i>
-              <a href="https://doi.org/10.1093/bioinformatics/bty407" target="_blank" rel="noreferrer noopener">Nextstrain: real-time tracking of pathogen evolution</a>
-              <i>, Bioinformatics</i> (2018)
-              {(typeof window !== 'undefined' && window.location.pathname.replace(/\//g, "")!=="team") && (
-                <>
-                  <div style={{margin: "10px 0px"}}/>
-                  The core Nextstrain team is
-                  <div style={{margin: "0px 0px"}}/>
-                  <FooterList />
-                  {"Please see the "}
-                  <a href="/team">team page</a>
-                  {" for more details."}
-                </>
-                )}
-            </Styles.IconParagraph>
-          </div>
-          <div className="col-lg-1"/>
+      <div className="row">
+        <div className="col-lg-1"/>
+        <div className="col-lg-10">
+          <Styles.IconParagraph>
+            {"Hadfield "}<i>{"et al., "}</i>
+            <a href="https://doi.org/10.1093/bioinformatics/bty407" target="_blank" rel="noreferrer noopener">Nextstrain: real-time tracking of pathogen evolution</a>
+            <i>, Bioinformatics</i> (2018)
+            {(typeof window !== 'undefined' && window.location.pathname.replace(/\//g, "")!=="team") && (
+              <>
+                <div style={{margin: "10px 0px"}}/>
+                The core Nextstrain team is
+                <div style={{margin: "0px 0px"}}/>
+                <FooterList />
+                {"Please see the "}
+                <a href="/team">team page</a>
+                {" for more details."}
+              </>
+              )}
+          </Styles.IconParagraph>
         </div>
+        <div className="col-lg-1"/>
+      </div>
 
-        <BigSpacer/>
+      <BigSpacer/>
 
-        <div className="row">
-          <div className="col-lg-12">
-            <Styles.WideParagraph>
-              All <a href="https://github.com/nextstrain">source code</a> is freely available under the terms of the <a href="https://github.com/nextstrain/auspice/blob/master/LICENSE.txt">GNU Affero General Public License</a>. Screenshots may be used under a <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY-4.0 license</a> and attribution to nextstrain.org must be provided.
-            </Styles.WideParagraph>
+      <div className="row">
+        <div className="col-lg-12">
+          <Styles.WideParagraph>
+            All <a href="https://github.com/nextstrain">source code</a> is freely available under the terms of the <a href="https://github.com/nextstrain/auspice/blob/master/LICENSE.txt">GNU Affero General Public License</a>. Screenshots may be used under a <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY-4.0 license</a> and attribution to nextstrain.org must be provided.
+          </Styles.WideParagraph>
 
-            <Styles.WideParagraph>
-              This work is made possible by the open sharing of genetic data by
-              research groups from all over the world. We gratefully acknowledge
-              their contributions. Special thanks to Kristian Andersen, Josh
-              Batson, David Blazes, Jesse Bloom, Peter Bogner, Anderson Brito,
-              Matt Cotten, Ana Crisan, Tulio de Oliveira, Gytis Dudas, Vivien
-              Dugan, Karl Erlandson, Nuno Faria, Jennifer Gardy, Nate Grubaugh,
-              Becky Kondor, Dylan George, Ian Goodfellow, Betz Halloran,
-              Christian Happi, Jeff Joy, Paul Kellam, Philippe Lemey, Nick
-              Loman, Duncan MacCannell, Erick Matsen, Sebastian Maurer-Stroh,
-              Placide Mbala, Danny Park, Oliver Pybus, Andrew Rambaut, Colin
-              Russell, Pardis Sabeti, Katherine Siddle, Kristof Theys, Dave
-              Wentworth, Shirlee Wohl and Cecile Viboud for comments,
-              suggestions and data sharing.
-            </Styles.WideParagraph>
-          </div>
-        </div>
-
-        <SmallSpacer/>
-
-        {/* FOOTER / LOGOS */}
-        <Logos />
-
-        <SmallSpacer/>
-
-        <div className="row">
-          <div className="col-lg-12">
-            <Styles.FooterParagraph>
-              © 2015–2023 Trevor Bedford and Richard Neher
-            </Styles.FooterParagraph>
-          </div>
+          <Styles.WideParagraph>
+            This work is made possible by the open sharing of genetic data by
+            research groups from all over the world. We gratefully acknowledge
+            their contributions. Special thanks to Kristian Andersen, Josh
+            Batson, David Blazes, Jesse Bloom, Peter Bogner, Anderson Brito,
+            Matt Cotten, Ana Crisan, Tulio de Oliveira, Gytis Dudas, Vivien
+            Dugan, Karl Erlandson, Nuno Faria, Jennifer Gardy, Nate Grubaugh,
+            Becky Kondor, Dylan George, Ian Goodfellow, Betz Halloran,
+            Christian Happi, Jeff Joy, Paul Kellam, Philippe Lemey, Nick
+            Loman, Duncan MacCannell, Erick Matsen, Sebastian Maurer-Stroh,
+            Placide Mbala, Danny Park, Oliver Pybus, Andrew Rambaut, Colin
+            Russell, Pardis Sabeti, Katherine Siddle, Kristof Theys, Dave
+            Wentworth, Shirlee Wohl and Cecile Viboud for comments,
+            suggestions and data sharing.
+          </Styles.WideParagraph>
         </div>
       </div>
-    );
-  }
+
+      <SmallSpacer/>
+
+      {/* FOOTER / LOGOS */}
+      <Logos />
+
+      <SmallSpacer/>
+
+      <div className="row">
+        <div className="col-lg-12">
+          <Styles.FooterParagraph>
+            © 2015–2023 Trevor Bedford and Richard Neher
+          </Styles.FooterParagraph>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
