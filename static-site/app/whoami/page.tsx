c15a78d0daaa999da9a47834db5e89f672b4b42c
John SJ Anderson <janders4@fredhutch.org>
2024-10-24 16:30:44 -0700

    Convert 'whoami' page to App Router [#134]
    Note: updates the page to use the default layout with the toolbar and
    team avatars, in addition to the sponsor logos.

---
+++ static-site/app/whoami/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import React, { useContext } from "react";
+
+import { UserContext } from "../../components/user-data-wrapper";
+
+import styles from "./styles.module.css";
+
+// we don't export a metadata object here because react client
+// components aren't allowed to do that, and we need to be a client
+// component to get access to the user context
+
+export default function WhoAmI(): React.ReactElement {
+  const { user, groupMemberships } = useContext(UserContext);
+
+  return user ? (
+    <div className={styles.userContainer}>
+      You’re logged in as <strong>{user.username}</strong>.
+      <p className={styles.subText}>
+        You are a member of the following Nextstrain groups, which each contain
+        a collection of datasets and/or narratives:
+      </p>
+      Public:
+      <ul className={styles.userGroupList}>
+        {groupMemberships
+          .filter((group) => group.isPublic)
+          .map((group) => (
+            <li key={group.name}>
+              <a href={`/groups/${group.name}`}>{group.name}</a>
+            </li>
+          ))}
+      </ul>
+      Private:
+      <ul className={styles.userGroupList}>
+        {groupMemberships
+          .filter((group) => !group.isPublic)
+          .map((group) => (
+            <li key={group.name}>
+              <a href={`/groups/${group.name}`}>{group.name}</a>
+            </li>
+          ))}
+      </ul>
+      <a href="/logout">Logout</a>
+    </div>
+  ) : (
+    <div className={styles.userContainer}>
+      <p>
+        You are not logged in.
+        <br />
+        <a href="/login">Login</a>
+      </p>
+    </div>
+  );
+}
--- static-site/pages/whoami.jsx
+++
@@ -1,3 +0,0 @@
-import dynamic from 'next/dynamic'
-const Index = dynamic(() => import("../src/pages/whoami"), {ssr: false})
-export default Index;
