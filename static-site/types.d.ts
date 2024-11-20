8ea90e59591e06fdf53dfdcbe6f7568b81423792
Victor Lin <13424970+victorlin@users.noreply.github.com>
2024-11-20 12:13:15 -0800

    Remove CSS/image static import types
    These are provided by Next.js in static-site/next-env.d.ts, which is
    generated during build.

--- static-site/types.d.ts
+++ static-site/types.d.ts
@@ -1,23 +1,3 @@
-declare module "*.css" {
-  const value: unknown;
-  export default value;
-}
-
-declare module "*.jpg" {
-  const value: { src: string };
-  export default value;
-}
-
-declare module "*.png" {
-  const value: { src: string };
-  export default value;
-}
-
-declare module "*.svg" {
-  const value: { src: string };
-  export default value;
-}
-
 declare module "*.yaml" {
   // Actual type should be set upon import via type assertion.
   const contents: unknown;
