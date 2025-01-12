637c1acf9db03cec80bfc53ed3bf3e20958a482a
BSPANGLER <bspangler21@alumni.uwosh.edu>
2025-01-12 00:29:06 -0600

    Use a media query to hide rainbowTitle on mobile devices

--- static-site/src/components/nav-bar/index.tsx
+++ static-site/src/components/nav-bar/index.tsx
@@ -98,23 +98,28 @@ const NavBar = ({ minified }: {
   }
 
   const LogoType = () => {
-    const title = "Nextstrain";
-    const rainbowTitle = title.split("").map((letter, i) =>
-      <NavLogoCharacter key={i} $colorIndex={i}>{letter}</NavLogoCharacter>
-    );
-    const SubTitle = styled.div`
-      color: black;
-    `;
-    return (
-      minified ?
-        <div/>
-        :
-        <a href="/">
-          {rainbowTitle}
-          {groupsApp &&
-            <SubTitle>Groups Server</SubTitle>}
-        </a>
-    );
+		const title = "Nextstrain";
+		const rainbowTitle = title.split("").map((letter, i) => (
+			<NavLogoCharacter key={i} $colorIndex={i}>
+				{letter}
+			</NavLogoCharacter>
+		));
+		const SubTitle = styled.div`
+			color: black;
+		`;
+		const ResponsiveTitle = styled.span`
+			@media (max-width: 768px) {
+				display: none;
+			}
+		`;
+		return minified ? (
+			<div />
+		) : (
+			<a href="/">
+				<ResponsiveTitle>{rainbowTitle}</ResponsiveTitle>
+				{groupsApp && <SubTitle>Groups Server</SubTitle>}
+			</a>
+		);
   }
 
   return (
