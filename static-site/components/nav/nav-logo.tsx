bbc3181d67683d0d84bfa7c96d060025e4dcdc3b
BSPANGLER <bspangler21@alumni.uwosh.edu>
2025-01-13 23:29:44 -0600

    Add code to hide site title when on mobile in the migrated version of the nav bar

--- static-site/components/nav/nav-logo.tsx
+++ static-site/components/nav/nav-logo.tsx
@@ -27,11 +27,15 @@ const Logo = (): React.ReactElement => (
 );
 
 const LogoType = (): React.ReactElement => {
-  const rainbowTitle = siteTitle
-    .split("")
-    .map((letter, i) => (
-      <span key={i} style={{ color: `var(--titleColor${i})` }}>{letter}</span>
-    ));
+  const rainbowTitle = siteTitle.split("").map((letter, i) => (
+		<span
+			key={i}
+			style={{ color: `var(--titleColor${i})` }}
+			className={styles.rainbowTitle}
+		>
+			{letter}
+		</span>
+  ));
   const SubTitle = (): React.ReactElement => (
     <div>Groups Server</div>
   );
