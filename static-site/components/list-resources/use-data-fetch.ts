5426b241711d5f9e2a94e9352fcef712ebd601ef
John SJ Anderson <janders4@fredhutch.org>
2025-02-13 11:21:54 -0800

    Port useDataFetch to App Router style [#1066]
    * convert to React Client Component
    * refactor code lightly

--- static-site/components/list-resources/use-data-fetch.ts
+++ static-site/components/list-resources/use-data-fetch.ts
@@ -1,30 +1,41 @@
-import { useState, useEffect } from 'react';
-import { Group, Resource, ResourceListingInfo } from './types';
-import { InternalError } from '../ErrorBoundary';
+"use client";
 
+import { useState, useEffect } from "react";
+
+import { InternalError } from "../error-boundary";
+
+import { Group, Resource, ResourceListingInfo } from "./types";
 
 /**
- * Uses the provided callback to fetch resources and parse those
- * resources into the `pathVersions` and `pathPrefix` values. The
- * callback is expected (and encouraged!) to throw() on any errors.
+ * React Hook that uses the provided `resourceListingCallback` to
+ * fetch resources and parse them into `pathVersions` and `pathPrefix`
+ * values. This callback is expected (and encouraged!) to `throw()` on
+ * any and all errors.
  *
- * Continues on to parse the `pathVersions`/`pathPrefix` data
- * structures into an array of groups, each representing a "pathogen"
- * and detailing the available resources for each, and the available
- * versions (snapshots) for each of those resources. In the case of
- * un-versioned resources, versions will be a zero-length array (i.e.,
- * `[]`)
+ * Then continues on to parse the `pathVersions`/`pathPrefix` data
+ * structures into a list of `Group` objects, each representing a
+ * "pathogen" or "group", and detailing the available resources for
+ * them, as well as the available versions (aka snapshots) for each of
+ * those resources. In the case of un-versioned resources (i.e., where
+ * the provided `versioned` boolean is false), `versions` will be an
+ * empty array (i.e., `[]`).
  *
- * The current implementation defines `versioned: boolean` for the
- * entire API response, however in the future we may shift this to the
- * API response and it may vary across the resources returned.
+ * The current implementation applies the provided `versioned` boolean
+ * to the entire API response. In the future, we may shift this
+ * `versioned` status into the API response proper, to allow it to
+ * vary across the returned resources.
+ *
+ * @param versioned - indicates whether the fetched resources are versioned
+ * @param defaultGroupLinks - whether to modify the group URL to include a group
+ * @param groupDisplayNames - a map used to optionally rename some resources
+ * @param resourceListingCallback - the callback to fetch resource data
  */
-export function useDataFetch(
+export default function useDataFetch(
   versioned: boolean,
   defaultGroupLinks: boolean,
   groupDisplayNames: Record<string, string>,
   resourceListingCallback: () => Promise<ResourceListingInfo>,
-) : {groups: Group[] | undefined, dataFetchError: boolean} {
+): { groups: Group[] | undefined; dataFetchError: boolean } {
   const [groups, setGroups] = useState<Group[]>();
   const [dataFetchError, setDataFetchError] = useState<boolean>(false);
 
@@ -33,11 +44,23 @@ export function useDataFetch(
       try {
         const { pathPrefix, pathVersions } = await resourceListingCallback();
 
-        /* group/partition the resources by pathogen (the first word
-        of the resource path). This grouping is constant for all UI
-        options so we do it a single time following the data fetch */
-        const partitions = partitionByPathogen(pathVersions, pathPrefix, versioned);
-        setGroups(groupsFrom(partitions, pathPrefix, defaultGroupLinks, groupDisplayNames));
+        // group/partition the resources by pathogen (the first word
+        // of the resource path). This grouping is constant for all UI
+        // options so we do it a single time following the data fetch
+        const partitions = _partitionByPathogen(
+          pathVersions,
+          pathPrefix,
+          versioned,
+        );
+
+        setGroups(
+          _groupsFrom(
+            partitions,
+            pathPrefix,
+            defaultGroupLinks,
+            groupDisplayNames,
+          ),
+        );
       } catch (err) {
         console.error(`Error while fetching and/or parsing data`);
         console.error(err);
@@ -46,151 +69,220 @@ export function useDataFetch(
     }
 
     fetchAndParse();
-  }, [versioned, defaultGroupLinks, groupDisplayNames, resourceListingCallback]);
+  }, [
+    versioned,
+    defaultGroupLinks,
+    groupDisplayNames,
+    resourceListingCallback,
+  ]);
 
-  return {groups, dataFetchError}
+  return { groups, dataFetchError };
 }
 
-
 /**
- * Groups the provided array mapping from path to dates into an object with keys
- * representing group names (pathogen names) and values which are arrays of
- * resource objects.
+ * Helper function to convert the provided `partitions` (an object
+ * mapping group names to lists of resources) into a list of `Group`
+ * objects
+ *
+ * @param partitions - object map of names to resources
+ * @param pathPrefix - prefix to add when defaultGroupLinks param is true
+ * @param defaultGroupLinks - boolean controlling conversion of groupUrl values
+ * @param groupDisplayNames - map controlling rename of resource names
  */
-function partitionByPathogen(
-  pathVersions: Record<string, string[]>,
-  pathPrefix: string,
-  versioned: boolean,
-) {
-  return Object.entries(pathVersions).reduce((store: Record<string, Resource[]>, [name, dates]) => {
-    const nameParts = name.split('/');
-
-    if (nameParts[0] === undefined) {
-      throw new InternalError(`Name is not properly formatted: '${name}'`);
-    }
-
-    const groupName = nameParts[0];
-
-    const resourceDetails: Resource = {
-      name,
-      groupName, /* decoupled from nameParts */
-      nameParts,
-      sortingName: _sortableName(nameParts),
-      url: `/${pathPrefix}${name}`,
-    };
-    if (versioned) {
-      const sortedDates = [...dates].sort();
-      if (sortedDates[0] === undefined) {
-        throw new InternalError("Resource does not have any dates.");
-      }
-
-      resourceDetails.lastUpdated = sortedDates.at(-1);
-      resourceDetails.firstUpdated = sortedDates[0];
-      resourceDetails.dates = sortedDates;
-      resourceDetails.nVersions = sortedDates.length;
-      resourceDetails.updateCadence = updateCadence(sortedDates.map((date)=> new Date(date)));
-    }
-
-    (store[groupName] ??= []).push(resourceDetails)
-
-    return store;
-  }, {});
-}
-
-
-/**
- * Turn the provided partitions (an object mapping groupName to an array of resources)
- * into an array of groups.
- */
-function groupsFrom(
+function _groupsFrom(
   partitions: Record<string, Resource[]>,
   pathPrefix: string,
   defaultGroupLinks: boolean,
   groupDisplayNames: Record<string, string>,
-) {
+): Group[] {
   return Object.entries(partitions).map(([groupName, resources]) => {
     const groupInfo: Group = {
       groupName: groupName,
       nResources: resources.length,
-      nVersions: resources.reduce((total, r) => r.nVersions ? total+r.nVersions : total, 0) || undefined,
-      lastUpdated: resources.map((r) => r.lastUpdated).sort().at(-1),
+      nVersions:
+        resources.reduce(
+          (total, r) => (r.nVersions ? total + r.nVersions : total),
+          0,
+        ) || undefined,
+      lastUpdated: resources
+        .map((r) => r.lastUpdated)
+        .sort()
+        .at(-1),
       resources,
-    }
-    /* add optional properties */
+    };
+
+    // add optional properties
     if (defaultGroupLinks) {
       groupInfo.groupUrl = `/${pathPrefix}${groupName}`;
     }
     if (groupDisplayNames?.[groupName]) {
       groupInfo.groupDisplayName = groupDisplayNames[groupName];
     }
+
     return groupInfo;
-  })
+  });
 }
 
+/**
+ * Helper function to group the provided `pathVersions` object
+ * mapping between path and dates into an object with pathogen/group
+ * names as the keys and arrays of Resource objects as the values.
+ *
+ * @param pathVersions - object mapping path to lists of dates
+ * @param pathPrefix - prefix added to resource name to build URL
+ * @param versioned - boolean controlling addition of version-specific fields
+ */
+function _partitionByPathogen(
+  pathVersions: Record<string, string[]>,
+  pathPrefix: string,
+  versioned: boolean,
+): Record<string, Resource[]> {
+  return Object.entries(pathVersions).reduce(
+    (store: Record<string, Resource[]>, [name, dates]) => {
+      const nameParts = name.split("/");
+
+      if (nameParts[0] === undefined) {
+        throw new InternalError(`Name is not properly formatted: '${name}'`);
+      }
+
+      const groupName = nameParts[0];
+
+      const resourceDetails: Resource = {
+        name,
+        groupName, // decoupled from nameParts
+        nameParts,
+        sortingName: _sortableName(nameParts),
+        url: `/${pathPrefix}${name}`,
+      };
+
+      if (versioned) {
+        const sortedDates = [...dates].sort();
+        if (sortedDates[0] === undefined) {
+          throw new InternalError("Resource does not have any dates.");
+        }
+
+        resourceDetails.lastUpdated = sortedDates.at(-1);
+        resourceDetails.firstUpdated = sortedDates[0];
+        resourceDetails.dates = sortedDates;
+        resourceDetails.nVersions = sortedDates.length;
+        resourceDetails.updateCadence = _updateCadence(
+          sortedDates.map((date) => new Date(date)),
+        );
+      }
+
+      (store[groupName] ??= []).push(resourceDetails);
+
+      return store;
+    },
+    {},
+  );
+}
 
 /**
- * Generally an alphabetical sorting of URL paths is a nice way to view the datasets,
- * however this results in temporal sorting such as [12y 2y 6m 6y] etc. This function
- * produces strings which, when sorted alphabetically, are in a nicer order.
+ * Helper function for sorting datasets in a way that is nicer than
+ * pure lexicographic sorting when dealing with temporal datasets
+ * names (e.g., "12y", "2y", "6m", etc.)
+ *
+ * @param words - list of strings to sort
  */
-function _sortableName(words: string[]) {
+function _sortableName(words: string[]): string {
   const w = words.map((word) => {
     const m = word.match(/^(\d+)([ym])$/);
     if (m && m[1]) {
-      if (m[2]==='y') return String(parseInt(m[1])*12).padStart(4,'0')
-      return m[1].padStart(4,'0')
+      if (m[2] === "y") {
+        return String(parseInt(m[1]) * 12).padStart(4, "0");
+      } else {
+        return m[1].padStart(4, "0");
+      }
     }
-    return word
-  })
-  return w.join("/")
+    return word;
+  });
+  return w.join("/");
 }
 
-
-const msInADay = 1000*60*60*24
+/** number of milliseconds in a day */
+const msInADay = 1000 * 60 * 60 * 24;
 
 /**
- * Considers the updates over the past two years and uses a heuristic based on
- * the median interval between updates to provide a summary of how frequently
- * the resource has been updated. Also considers the consistency (essentially
- * variance) of these updates, and whether the dataset continues to be updated.
+ * Helper function to that generates a update frequency information
+ * for a resource or dataset, based on consider a set of dates
+ * (corresponding to resource/dataset updates) over the past two
+ * years, and applying a heuristic based on the median interval
+ * between updates. Also considers consistency (essentially, the
+ * variance) of the updates, and whether the resource/dataset
+ * continues to be updated
+ *
+ * @param dateObjects - a list of Date objects corresponding to update dates
  */
-function updateCadence(dateObjects) {
+function _updateCadence(dateObjects: Date[]): {
+  summary: string;
+  description: string;
+} {
   const threshold = new Date(); // today
-  threshold.setFullYear(threshold.getFullYear()-2)
+  threshold.setFullYear(threshold.getFullYear() - 2);
 
-  const intervals = dateObjects.reduce((intervals, date, idx) => {
-    if (idx===0 || date < threshold) return intervals;
-    // get num days between subsequent dates, ignoring complexities such as DST
-    intervals.push(Math.round((date - dateObjects[idx-1])/msInADay))
-    return intervals;
-  }, []).sort((a, b) => a-b);
+  const intervals = dateObjects
+    .reduce((intervals: number[], date, idx) => {
+      if (idx === 0 || date < threshold) {
+        return intervals;
+      }
+      // get num days between subsequent dates, ignoring complexities such as DST
+      const prevDate = dateObjects[idx - 1] as Date; // eslint-disable-line @typescript-eslint/consistent-type-assertions
+      intervals.push(Math.round((date.getTime() - prevDate.getTime()) / msInADay));
 
-  if (!intervals.length) return {summary: "rarely", description: "This dataset hasn't seen any updates in the past 2 years."};
+      return intervals;
+    }, [])
+    .sort((a, b) => a - b);
+
+  if (!intervals.length)
+    return {
+      summary: "rarely",
+      description: "This dataset hasn't seen any updates in the past 2 years.",
+    };
 
   if (intervals.length < 3) {
-    return {summary: "rarely", description: `This dataset has been updated ${intervals.length+1} times in the past 2 years.`};
+    return {
+      summary: "rarely",
+      description: `This dataset has been updated ${intervals.length + 1} times in the past 2 years.`,
+    };
   }
 
-  const lastUpdateDaysAgo = Math.round(((new Date()).getTime() - dateObjects.at(-1))/msInADay);
-  const median = intervals[Math.floor(intervals.length/2)];
-  const mad = intervals.map((x) => Math.abs(x-median)).sort((a, b) => a-b)[Math.floor(intervals.length/2)]
+  // typechecker is not quite smart enough to figure out that:
+  // * there will always be a final object in `dateObjects`
+  // * `intervals[]` will always have a middle entry and it will be a number
+  // * for similar reasons, `mad` will always be a number
+  const lastUpdate = dateObjects.at(-1) as Date; // eslint-disable-line @typescript-eslint/consistent-type-assertions
+  const lastUpdateDaysAgo = Math.round(
+    (new Date().getTime() - lastUpdate.getTime()) / msInADay,
+  );
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  const median = intervals[Math.floor(intervals.length / 2)] as number;
+  // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+  const mad = intervals.map((x) => Math.abs(x - median)).sort((a, b) => a - b)[
+    Math.floor(intervals.length / 2)
+  ] as number;
 
   let description: string;
-  description = `Over the past two years this dataset's been updated ${mad*2 > median ? 'irregularly' : 'consistently'} every `;
-  description += median===1 ? '~day' : `~${median} days`;
-  if (lastUpdateDaysAgo > median*3) description+=", however it hasn't been updated for a while";
-  description += '.';
+  description = `Over the past two years this dataset's been updated ${mad * 2 > median ? "irregularly" : "consistently"} every `;
+  description += median === 1 ? "~day" : `~${median} days`;
+  if (lastUpdateDaysAgo > median * 3)
+    description += ", however it hasn't been updated for a while";
+  description += ".";
 
+  let summary: string;
   if (median <= 1) {
-    return {summary: "daily", description};
+    summary = "daily";
   } else if (median < 4) {
-    return {summary: "every few days", description};
+    summary = "every few days";
   } else if (median < 10) {
-    return {summary: "weekly", description};
+    summary = "weekly";
   } else if (median < 18) {
-    return {summary: "fortnightly", description};
+    summary = "fortnightly";
   } else if (median < 40) {
-    return {summary: "monthly", description};
+    summary = "monthly";
+  } else {
+    summary = "rarely";
   }
-  return {summary: "rarely", description};
+
+  return { summary, description };
 }
