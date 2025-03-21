ec79b187a79a07f543d5dfc514083faf2ade522b
John SJ Anderson <janders4@fredhutch.org>
2025-03-21 15:26:34 -0700

    Convert /pathogens over to using splat path to display error message... [#1133]
    ...for a invalid request path.

---
+++ static-site/app/pathogens/[[...pathogens]]/not-found.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import PathogensPageContent from "./content";
+import ErrorBanner from "./error-banner";
+import { metadata } from "./page";
+
+/**
+ * A React Server component that renders the usual `/staging` page
+ * content, with an error banner up-top explaining that the requested
+ * dataset doesn't actuall exist.
+ */
+export default function FourOhFour(): React.ReactElement {
+  return (
+    <>
+      <ErrorBanner />
+      <PathogensPageContent metadata={metadata} />
+    </>
+  );
+}
