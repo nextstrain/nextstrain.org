9bcfc30c24d89f7021ffeeabf98e9cf09ddb791f
John SJ Anderson <janders4@fredhutch.org>
2025-03-21 12:21:15 -0700

    Remediate comment in `static-site/components/line` [#1133]

--- static-site/components/line/index.tsx
+++ static-site/components/line/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import styles from "./styles.module.css";
 
+/** A React Server Component that renders a div styled to provide a horizontal rule */
 export default function Line (): React.ReactElement {
   return <div className={styles.line}></div>
 }
