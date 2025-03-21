6cf6a41685a4e0ba4ec60821da6162680ebfab4d
John SJ Anderson <janders4@fredhutch.org>
2025-03-21 12:29:45 -0700

    Remediate comments in `static-site/components/nav` tree [#1133]

--- static-site/components/nav/nav-logo.tsx
+++ static-site/components/nav/nav-logo.tsx
@@ -9,6 +9,12 @@ import logo from "../../static/logos/nextstrain-logo-small.png";
 
 import styles from "./styles.module.css";
 
+/**
+ * React Client Component to render the nav bar logo on everything
+ * except the home page.
+ *
+ * Needs to be a client component because of the use of `usePathname()`
+ */
 export default function NavLogo(): React.ReactElement | null {
   const pathname = usePathname();
 
@@ -20,21 +26,23 @@ export default function NavLogo(): React.ReactElement | null {
   ) : null;
 }
 
-const Logo = (): React.ReactElement => (
-  <a href="/" className={styles.logo}>
-    <Image src={logo} alt="Nextstrain (logo)" width="40" height="40" />
-  </a>
-);
-
-const LogoType = (): React.ReactElement => {
-  const rainbowTitle = siteTitle
-    .split("")
-    .map((letter, i) => (
-      <span key={i} style={{ color: `var(--titleColor${i})` }}>{letter}</span>
-    ));
-  const SubTitle = (): React.ReactElement => (
-    <div>Groups Server</div>
+/** React Client Component that renders the Nextstrain logo */
+function Logo(): React.ReactElement {
+  return (
+    <a href="/" className={styles.logo}>
+      <Image src={logo} alt="Nextstrain (logo)" width="40" height="40" />
+    </a>
   );
+}
+
+/** React Client Component that renders the title below the logo */
+function LogoType(): React.ReactElement {
+  const rainbowTitle = siteTitle.split("").map((letter, i) => (
+    <span key={i} style={{ color: `var(--titleColor${i})` }}>
+      {letter}
+    </span>
+  ));
+  const SubTitle = (): React.ReactElement => <div>Groups Server</div>;
 
   return (
     <div className={styles.responsiveTitle}>
@@ -44,4 +52,4 @@ const LogoType = (): React.ReactElement => {
       </a>
     </div>
   );
-};
+}
