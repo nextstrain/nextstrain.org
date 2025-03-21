1a546f21b28a6b61bda763625cebf653317c97d5
John SJ Anderson <janders4@fredhutch.org>
2025-03-21 12:19:28 -0700

    Remediate comments in `static-site/components/focus-paragraph` [#1133]

--- static-site/components/focus-paragraph/index.tsx
+++ static-site/components/focus-paragraph/index.tsx
@@ -2,17 +2,24 @@ import React from "react";
 
 import styles from "./styles.module.css";
 
+/** A React Server Component that renders its children inside a centered paragraph */
 export function FocusParagraphCentered({
   children,
 }: {
+  /** the child nodes to render in the paragraph */
   children: React.ReactNode;
 }): React.ReactElement {
   return <p className={`${styles.focus} ${styles.centered}`}>{children}</p>;
 }
 
+/**
+ * A React Server Component that renders its children inside a
+ * paragraph that is slightly narrower than the default
+ */
 export function FocusParagraphNarrow({
   children,
 }: {
+  /** the child nodes to render in the paragraph */
   children: React.ReactNode;
 }): React.ReactElement {
   return <p className={`${styles.focus} ${styles.narrow}`}>{children}</p>;
