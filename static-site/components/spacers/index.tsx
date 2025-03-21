a8cd52efead27a33f55474ee80c6ce36d623aa04
John SJ Anderson <janders4@fredhutch.org>
2025-03-21 12:32:09 -0700

    Remediate comments in `static-site/comments/spacers` [#1133]

--- static-site/components/spacers/index.tsx
+++ static-site/components/spacers/index.tsx
@@ -7,11 +7,20 @@ const DEFAULT_HEIGHTS = {
   huge: 40,
 };
 
+/**
+ * React Server Component that renders a spacer div with a particular height
+ *
+ * Note that this is an internal, non-exported helper to keep the
+ * actually exported components DRY
+ */
 function Spacer({
   count,
   height,
 }: {
+  /** how many spacers? (optional, defaults to 1) */
   count?: number;
+
+  /** height of an individual spacer */
   height: number;
 }): React.ReactElement {
   if (!count) {
@@ -21,17 +30,41 @@ function Spacer({
   return <div style={{ height: count * height }}></div>;
 }
 
-export function SmallSpacer({ count }: { count?: number }): React.ReactElement {
+/** React Server Component for a small spacer */
+export function SmallSpacer({
+  count,
+}: {
+  /** (optional) number of spacers to return, default 1 */
+  count?: number;
+}): React.ReactElement {
   return <Spacer count={count} height={DEFAULT_HEIGHTS["small"]} />;
 }
 
-export function MediumSpacer({ count }: { count?: number }): React.ReactElement {
+/** React Server Component for a medium spacer */
+export function MediumSpacer({
+  count,
+}: {
+  /** (optional) number of spacers to return, default 1 */
+  count?: number;
+}): React.ReactElement {
   return <Spacer count={count} height={DEFAULT_HEIGHTS["medium"]} />;
 }
 
-export function BigSpacer({ count }: { count?: number }): React.ReactElement {
+/** React Server Component for a big spacer */
+export function BigSpacer({
+  count,
+}: {
+  /** (optional) number of spacers to return, default 1 */ count?: number;
+}): React.ReactElement {
   return <Spacer count={count} height={DEFAULT_HEIGHTS["big"]} />;
 }
-export function HugeSpacer({ count }: { count?: number }): React.ReactElement {
+
+/** React Server Component for a HUGE spacer */
+export function HugeSpacer({
+  count,
+}: {
+  /** (optional) number of spacers to return, default 1 */
+  count?: number;
+}): React.ReactElement {
   return <Spacer count={count} height={DEFAULT_HEIGHTS["huge"]} />;
 }
