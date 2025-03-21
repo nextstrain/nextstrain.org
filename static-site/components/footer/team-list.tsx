a6e9c56db40b934591580da7c99665fd783b3465
John SJ Anderson <janders4@fredhutch.org>
2025-03-21 12:20:51 -0700

    Remediate comments and whitespace in `static-site/components/footer` [#1133]

--- static-site/components/footer/team-list.tsx
+++ static-site/components/footer/team-list.tsx
@@ -1,16 +1,20 @@
 "use client";
 
-// Because we don't want to show the footer version of the team list
-// on the team page, this component needs access to the pathname, so
-// it can decide whether or not to show the list in the footer --
-// which means it needs to be a client component.
-
 import { usePathname } from "next/navigation";
 import React from "react";
 
 import { MediumSpacer } from "../../src/layouts/generalComponents";
 import { FooterTeamList } from "../people/avatars";
 
+/**
+ * A React Client component that displays the core Nextstrain team in
+ * the footer on every page except the /team page
+ *
+ * Because we don't want to show the footer version of the team list
+ * on the team page, this component needs access to the pathname, so
+ * it can decide whether or not to show the list in the footer --
+ * which means it needs to be a client component.
+ */
 export default function TeamList() {
   const pathname = usePathname();
 
