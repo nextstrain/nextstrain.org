3b8640f9557c8cc898183c9b44c9c7e6ac67f995
John SJ Anderson <janders4@fredhutch.org>
2025-03-21 12:18:53 -0700

    Remediate comments in `static-site/components/flex-center` [#1133]

--- static-site/components/flex-center/index.tsx
+++ static-site/components/flex-center/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 import styles from "./styles.module.css";
 
+/** A React Server Component that formats its child nodes as a centered flex-div */
 export default function FlexCenter({
   children,
 }: {
+  /** the child nodes to render in the flex-div */
   children: React.ReactNode;
 }): React.ReactElement {
   return <div className={styles.flexCenter}>{children}</div>;
