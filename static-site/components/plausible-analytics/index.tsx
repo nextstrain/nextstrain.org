23d672188ecfc27cd235414f6048619bffe6e126
John SJ Anderson <janders4@fredhutch.org>
2025-04-30 09:37:52 -0700

    Make use of Plausible Analytics configurable [#1162]
    Setting the config values to `undefined` will disable inclusion of the
    analytics script completely.

--- static-site/components/plausible-analytics/index.tsx
+++ static-site/components/plausible-analytics/index.tsx
@@ -1,17 +1,31 @@
 import Script from "next/script";
 import React from "react";
 
+// import from BaseConfig to avoid a conflict with SiteConfig, due to
+// it importing an older component that's still using styled.
+//
+// TODO convert over to SiteConfig once this is no longer true.
+import {
+  plausibleAnalyticsDataDomain,
+  plausibleAnalyticsScript,
+} from "../../data/BaseConfig";
+
 /** React Server Component to add analytics javascript */
 export default function PlausibleAnalytics(): React.ReactElement {
-  /* See <https://plausible.io/docs/plausible-script>. Analytics are disabled on
-     localhost, but use `script.local.js` if you want to track localhost for
-     testing purposes */
-  return (
-    <Script
-      src="https://plausible.io/js/script.js"
-      data-domain="nextstrain.org"
-      async
-      defer
-    />
-  );
+  /* See <https://plausible.io/docs/plausible-script>. Analytics are
+     disabled on localhost; see comments in BaseConfig if you want to
+     change configuration, enable analytics for localhost, or disable
+     them completely. */
+  if (plausibleAnalyticsDataDomain && plausibleAnalyticsScript) {
+    return (
+      <Script
+        src={plausibleAnalyticsScript}
+        data-domain={plausibleAnalyticsDataDomain}
+        async
+        defer
+      />
+    );
+  } else {
+    return <></>;
+  }
 }
