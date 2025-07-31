71829af79e485058500adb3608038243f149d060
John SJ Anderson <janders4@fredhutch.org>
2025-07-31 12:29:53 -0700

    Port `/community` to App Router style [#1149]
    * Remove the base `community/page.tsx` in favor of doing everything in
      `[[...community]]/page.tsx` — this reduces the amount of duplicated
      code in the different rendering paths
    * Refactor `community-repo-page.tsx` to use CSS Module instead of
      Styled Components
    * Refactored the "main versus repo with or without error banner" logic
      into `<CommunityPageRouter>`
    * Refactored the base page into a Server Component (to support use of
      the Metadata API) with all the client-side stuff refactored into the
      `<CommunityPageRouter>` and `<CommunityPageContent>` Client Components

--- static-site/app/community/[[...community]]/community-repo-page.tsx
+++ static-site/app/community/[[...community]]/community-repo-page.tsx
@@ -1,152 +1,279 @@
-import React from "react";
-import ScrollableAnchor, { configureAnchors } from '../../vendored/react-scrollable-anchor/index';
-import { HugeSpacer } from "../layouts/generalComponents";
-import * as splashStyles from "../components/splash/styles";
-import DatasetSelect from "../components/Datasets/dataset-select";
-import GenericPage from "../layouts/generic-page";
-import { fetchAndParseJSON } from "../util/datasetsHelpers";
-import { ErrorBanner } from "../components/errorMessages";
-import SourceInfoHeading from "../components/sourceInfoHeading";
+import React, { useEffect, useState } from "react";
 
-class Index extends React.Component {
-  constructor(props) {
-    super(props);
-    configureAnchors({ offset: -10 });
-    this.state = {
-      repoNotFound: false,
-    };
-  }
+import DatasetSelect from "../../../components/dataset-select";
+import { DatasetType } from "../../../components/dataset-select/types";
+import ErrorMessage from "../../../components/error-message";
+import SourceInfoHeading, {
+  SourceInfo,
+} from "../../../components/source-info-heading";
+import { HugeSpacer } from "../../../components/spacers";
+import { fetchAndParseJSON } from "../../../src/util/datasetsHelpers";
 
-  // parse getAvailable listing into one that dataset-select component accepts
-  createDatasetListing = (list, userName) => {
-    if (!list) return [];
-
-    // Note that the `request` always includes @branch, irregardless of URL.
-    return list.map((d) => {
-      return {
-        // filename will be used by <DatasetSelect> as the display name
-        filename: d.request.replace(/\/?(community\/)?(narratives\/)?[^/]+\/[^/]+\/?/, ''),
-        url: `/${d.request}`,
-        contributor: userName
-      };
-    });
-  };
-
-  async componentDidMount() {
-    const {userName, repoName} = this.props;
-    try {
-      const [sourceInfo, availableData] = await Promise.all([
-        fetchAndParseJSON(`/charon/getSourceInfo?prefix=/community/${userName}/${repoName}/`),
-        fetchAndParseJSON(`/charon/getAvailable?prefix=/community/${userName}/${repoName}/`)
-      ]);
-      this.setState({
-        sourceInfo,
-        userName,
-        repoName,
-        datasets: this.createDatasetListing(availableData.datasets, userName),
-        narratives: this.createDatasetListing(availableData.narratives, userName),
-      });
-    } catch (err) {
-      console.error("Cannot find user/repo.", err.message);
-      this.setState({userName, repoName, repoNotFound: true});
-    }
-  }
-
-  banner() {
-    const {isNarrative, userName, repoName, resourcePath, nonDefaultResourcePathParts} = this.props;
-    let bannerTitle, bannerContents;
-    // Set up a banner if we have additional resource path parts dataset or narrative doesn't exist
-    if (isNarrative || nonDefaultResourcePathParts.length) {
-      bannerTitle = isNarrative
-        ? `The narrative "nextstrain.org/${resourcePath}" doesn't exist.`
-        : `The dataset "nextstrain.org/${resourcePath}" doesn't exist.`;
-      bannerContents = `Here is the page for the "${userName}/${repoName}" repository.`;
-    }
-    // Set up a banner or update the existing one if the repo doesn't exist
-    if (this.state.repoNotFound) {
-      const notFound = (<>
-        The GitHub repository <a href={`https://github.com/${userName}/${repoName}`}>{userName}/{repoName}</a> {"doesn't"} exist (or is private), or there was an error getting data for that repository.
-      </>);
-      const description = (<>
-        <p>If {"you're"} setting up your own Community on GitHub repository, see <a href="https://docs.nextstrain.org/en/latest/guides/share/community-builds.html">our documentation</a>.</p>
-        <p>For a list of featured Nextstrain Community datasets, check out the <a href="/community">Community page</a>.</p>
-      </>);
-      if (!bannerTitle) {
-        bannerTitle = notFound;
-        bannerContents = description;
-      } else {
-        bannerContents = (<>
-          <p>{notFound}</p>
-          {description}
-        </>);
-      }
-    }
-    return bannerTitle ? <ErrorBanner title={bannerTitle} contents={bannerContents}/> : null;
-  }
-
-  render() {
-    const banner = this.banner();
-    if (this.state.repoNotFound) {
-      return (
-        <GenericPage banner={banner} />
-      );
-    }
-    if (!this.state.sourceInfo) {
-      return (
-        <GenericPage banner={banner}>
-          <splashStyles.H2>Data loading...</splashStyles.H2>
-        </GenericPage>
-      );
-    }
-    return (
-      <GenericPage banner={banner}>
-        <SourceInfoHeading sourceInfo={this.state.sourceInfo}/>
-        <HugeSpacer />
-        {this.state.sourceInfo.showDatasets && (
-          <ScrollableAnchor id={"datasets"}>
-            <div>
-              <splashStyles.H3>Available datasets</splashStyles.H3>
-              {this.state.datasets.length === 0 ?
-                <splashStyles.H4>No datasets are available for this repo.</splashStyles.H4> :
-                <DatasetSelect
-                  datasets={this.state.datasets}
-                  columns={[
-                    {
-                      name: "Dataset",
-                      value: (dataset) => dataset.filename.replace(/_/g, ' / ').replace('.json', ''),
-                      url: (dataset) => dataset.url
-                    }
-                  ]}
-                />
-              }
-            </div>
-          </ScrollableAnchor>
-        )}
-        <HugeSpacer />
-        {this.state.sourceInfo.showNarratives && (
-          <ScrollableAnchor id={"narratives"}>
-            <div>
-              <splashStyles.H3>Available narratives</splashStyles.H3>
-              {this.state.narratives.length === 0 ?
-                <splashStyles.H4>No narratives are available for this repo.</splashStyles.H4> :
-                <DatasetSelect
-                  datasets={this.state.narratives}
-                  columns={[
-                    {
-                      name: "Narrative",
-                      value: (dataset) => dataset.filename.replace(/_/g, ' / ').replace('.json', ''),
-                      url: (dataset) => dataset.url
-                    }
-                  ]}
-                  title="Filter Narratives"
-                />
-              }
-            </div>
-          </ScrollableAnchor>
-        )}
-      </GenericPage>
-    );
-  }
+/** Data structure for `/charon/getAvailable` response */
+interface AvailableData {
+  /** list of available datasets */
+  datasets: CommunityResource[];
+  /** list of available narratives */
+  narratives: CommunityResource[];
 }
 
-export default Index;
+/**
+ * Data structure for an individual dataset in the
+ * `/charon/getAvailable` response
+ */
+interface CommunityResource {
+  /** the path to request to load the data set */
+  request: string;
+}
+
+/**
+ * A React Server Component for displaying a page for a community
+ * repo, with metadata about the repo and user, and listing of
+ * available datasets and narratives in that repo.
+ */
+export default function CommunityRepoPage({
+  user,
+  repo,
+  extra,
+  isNarrative,
+}: {
+  /** Github user name */
+  user: string;
+  /** Github repo name */
+  repo: string;
+  /** Any extra elements in the requested URL past the user and repo */
+  extra: string;
+  /** Was the request for a narrative? (i.e., did `/narratives/`
+   * appear in the URL?)
+   */
+  isNarrative: boolean;
+}): React.ReactElement {
+  // these flags control what's displayed: the repo content and/or an error banner
+  const [showContent, setShowContent] = useState<boolean>(false);
+  const [showError, setShowError] = useState<boolean>(false);
+
+  // this is to store the information returned from the
+  // `/charon/getSourceInfo` endpoint; we initialize the state to
+  // essentially an empty data structure
+  const [sourceInfo, setSourceInfo] = useState<SourceInfo>({
+    title: "",
+    byline: "",
+    website: null,
+    showDatasets: false,
+    showNarratives: false,
+    avatar: "",
+  });
+
+  // these will hold the datasets and narratives that are parsed out
+  // of the response from the `/charon/getAvailable` endpoint
+  const [datasets, setDatasets] = useState<DatasetType[]>([]);
+  const [narratives, setNarratives] = useState<DatasetType[]>([]);
+
+  // this hook uses the internal (infernal?) Charon API to get
+  // information about the Github user/repo in the requested path
+  useEffect((): void => {
+    async function fetchRepoAndParse(): Promise<void> {
+      if (!user || !repo) {
+        // this shouldn't ever happen, because we should only try to
+        // render this component when we know we have both a user and
+        // a repo
+        throw new Error("we should have a user and a repo at this point…");
+      }
+
+      try {
+        const [fetchedSourceInfo, fetchedAvailableData]: [
+          SourceInfo,
+          AvailableData,
+        ] = await Promise.all([
+          fetchAndParseJSON(
+            `/charon/getSourceInfo?prefix=/community/${user}/${repo}/`,
+          ),
+          fetchAndParseJSON(
+            `/charon/getAvailable?prefix=/community/${user}/${repo}/`,
+          ),
+        ]);
+
+        setShowContent(true);
+        setSourceInfo(fetchedSourceInfo);
+        setDatasets(
+          _createDatasetListing({
+            list: fetchedAvailableData.datasets,
+            user,
+          }),
+        );
+        setNarratives(
+          _createDatasetListing({
+            list: fetchedAvailableData.narratives,
+            user,
+          }),
+        );
+      } catch (err) {
+        console.error(
+          "Cannot find user/repo.",
+          err instanceof Error ? err.message : null,
+        );
+
+        // if the Github content fetch failed, _only_ show an error box
+        setShowError(true);
+      }
+    }
+
+    fetchRepoAndParse();
+  }, [user, repo]);
+
+  let errorBannerTitle: string | React.ReactElement = "";
+  let errorBannerContents: React.ReactElement = <></>;
+
+  // if we're rendering a `/community/narratives/user/repo` or a
+  // `/community/user/repo/extra/stuff` request, we want to display an
+  // error banner saying that thing doesn't exist (because if it did,
+  // the Express router would have handled it instead of this
+  // component)
+  if (isNarrative || extra) {
+    const resourceType = isNarrative ? "narrative" : "dataset";
+    const resourcePath = `nextstrain.org/community/${isNarrative ? "narratives/" : ""}${user}/${repo}/${extra}`;
+
+    errorBannerTitle = `The ${resourceType} "${resourcePath}" doesn't exist.`;
+    errorBannerContents = (
+      <p>
+        Here is the page for the &ldquo;{user}/{repo}&rdquo; repository.
+      </p>
+    );
+  }
+
+  // if the `showError` flag is set, that means we couldn't even find
+  // `github.com/user/repo`, so show an error banner that says that
+  if (showError) {
+    const repoNotFound = (
+      <>
+        The GitHub repository{" "}
+        <a href={`https://github.com/${user}/${repo}`}>
+          {user}/{repo}
+        </a>{" "}
+        doesn&apos;t exist (or is private), or there was an error getting data
+        for that repository.
+      </>
+    );
+    const settingUpHelp = (
+      <>
+        <p>
+          If you&apos;re setting up your own Community on GitHub repository, see{" "}
+          <a href="https://docs.nextstrain.org/en/latest/guides/share/community-builds.html">
+            our documentation
+          </a>
+          .
+        </p>
+        <p>
+          For a list of featured Nextstrain Community datasets, check out the{" "}
+          <a href="/community">Community page</a>.
+        </p>
+      </>
+    );
+
+    if (errorBannerTitle) {
+      // we already set up a banner in the first "is this a request
+      // that should have been handled at the Express level" check, so
+      // amend it
+      errorBannerContents = (
+        <>
+          <p>{repoNotFound}</p>
+          {settingUpHelp}
+        </>
+      );
+    } else {
+      // no other errors, just show the "repo not found" ones
+      errorBannerTitle = repoNotFound;
+      errorBannerContents = settingUpHelp;
+    }
+  }
+
+  return (
+    <>
+      {errorBannerTitle && (
+        <ErrorMessage title={errorBannerTitle} contents={errorBannerContents} />
+      )}
+
+      <HugeSpacer />
+
+      {showContent && (
+        <>
+          <HugeSpacer />
+          <SourceInfoHeading sourceInfo={sourceInfo}></SourceInfoHeading>
+          <HugeSpacer />
+          {sourceInfo.showDatasets && (
+            <AvailableDatasets datasets={datasets} kind="dataset" />
+          )}
+          <HugeSpacer />
+          {sourceInfo.showNarratives && (
+            <AvailableDatasets datasets={narratives} kind="narrative" />
+          )}
+        </>
+      )}
+    </>
+  );
+}
+
+/**
+ * A helper React Component to display available datasets of a
+ * particular kind in a <DatasetSelect> (or to display a message that
+ * there aren't any datasets of that kind to display)
+ */
+function AvailableDatasets({
+  datasets,
+  kind,
+}: {
+  datasets: DatasetType[];
+  kind: string;
+}): React.ReactElement {
+  const titleKind = kind[0]?.toUpperCase() + kind.slice(1);
+
+  return (
+    <div>
+      <h3 className="centered">Available {kind}s</h3>
+      {datasets.length === 0 ? (
+        <h4 className="centered">
+          No {kind}s are available for this repo.
+        </h4>
+      ) : (
+        <DatasetSelect
+          datasets={datasets}
+          columns={[
+            {
+              name: titleKind,
+              value: (dataset) =>
+                dataset.filename?.replace(/_/g, " / ").replace(".json", "") ||
+                "",
+              url: (dataset) => dataset.url,
+            },
+          ]}
+          title={`Filter ${titleKind}s`}
+        />
+      )}
+    </div>
+  );
+}
+
+// helper to map the `/charon/getAvailable` response into a list of
+// DatasetType objects
+function _createDatasetListing({
+  list,
+  user,
+}: {
+  list: CommunityResource[];
+  user: string;
+}): DatasetType[] {
+  if (!list) return [];
+
+  // Note that the `request` always includes @branch, irregardless of URL.
+  return list.map((d: CommunityResource): DatasetType => {
+    return {
+      // filename will be used by <DatasetSelect> as the display name
+      filename: d.request.replace(
+        /\/?(community\/)?(narratives\/)?[^/]+\/[^/]+\/?/,
+        "",
+      ),
+      url: `/${d.request}`,
+      contributor: user,
+    };
+  });
+}
