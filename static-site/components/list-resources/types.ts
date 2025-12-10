43450f9908cad1bea70d8ecc6743ec599490e58f
Victor Lin <13424970+victorlin@users.noreply.github.com>
2025-12-10 11:41:49 -0800

    Support multiple resource types
    This adds a new property for filter type when creating filter options,
    and includes it at the top of the dropdown synced with a new URL search
    param 'type'.
    
    The property is optional for backwards compatibility, defaulting to a
    filter type of "namePart" when unset.

--- static-site/components/list-resources/types.ts
+++ static-site/components/list-resources/types.ts
@@ -4,7 +4,10 @@ import { GenericTileBase } from "../expandable-tiles/types";
 export interface FilterOption {
   value: string;
   label: string;
+  filterType: FilterType;
 }
+
+export type FilterType = 'namePart' | 'resourceType';
 
 export type SortMethod = "lastUpdated" | "alphabetical";
 
@@ -66,6 +69,7 @@ export interface Resource {
 
   sortingName: string;
   url: string;
+  resourceType?: ResourceType;
   lastUpdated?: string; // date
   firstUpdated?: string; // date
   dates?: string[];
@@ -94,7 +98,9 @@ export interface ResourceDisplayName {
   default: string;
 }
 
-export type ResourceType = 'dataset'|'intermediate';
+export type ResourceType = 'resource'|'dataset'|'intermediate'|'narrative';
+export const RESOURCE_TYPES: ResourceType[] = ['resource', 'dataset', 'intermediate', 'narrative'];
+// NOTE: the resource indexer only uses a subset of these
 
 
 export interface UpdateCadence {
