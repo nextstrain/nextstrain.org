dc9fd67761fc936d8325bcb085de2c18e8860649
Victor Lin <13424970+victorlin@users.noreply.github.com>
2026-01-20 13:25:23 -0800

    🚧 simplify "nextstrain" collection check
    Depends on other code changes to preserve the "nextstrain/" prefix.

--- static-site/app/nextclade/resources.tsx
+++ static-site/app/nextclade/resources.tsx
@@ -8,8 +8,6 @@ import { listResourcesAPI } from "../../components/list-resources/listResourcesA
 import { Group } from "../../components/list-resources/types";
 import nextstrainLogoSmall from "../../static/logos/nextstrain-logo-small.png";
 
-const NON_NEXTSTRAIN_COLLECTIONS = ["community", "enpen"];
-
 export default function NextcladeResourceListing(): React.ReactElement {
   return (
     <ListResources
@@ -26,46 +24,29 @@ async function nextcladeDatasetResourceGroups() {
   return await listResourcesAPI('nextclade', 'dataset', {
     versioned: true,
 
-    /* For non-Nextstrain "<collection>/a/b/c", use "<collection>/a" as the
-     * grouping instead of just "<collection>".
+    /* For Nextstrain "nextstrain/a/b/c", use "a" as the grouping (stripping
+     * the "nextstrain/" prefix for display). For other collections like
+     * "<collection>/a/b/c", use "<collection>/a" as the grouping.
      */
     groupNameBuilder: (name: string): string => {
-      return isNextstrain(name)
-        ? name.split("/")[0]!  // eslint-disable-line @typescript-eslint/no-non-null-assertion
+      return name.startsWith("nextstrain/")
+        ? name.split("/")[1]!  // eslint-disable-line @typescript-eslint/no-non-null-assertion
         : name.split("/").slice(0, 2).join("/");
     },
 
     // Sort non-Nextstrain datasets after ours
     groupSortableName: (group: Group): string => {
-      const name = group.groupName;
-      return isNextstrain(name) ? `000 ${name}` :
-                                  `001 ${name}` ;
+      const isOfficialDataset = group.resources.some((r) => r.name.startsWith("nextstrain/"));
+      return isOfficialDataset ? `000 ${group.groupName}` :
+                                 `001 ${group.groupName}` ;
     },
 
     // Add Nextstrain logo for core datasets
     groupImg: (group: Group) => {
-      const isOfficialDataset = group.resources.some((r) => isNextstrain(r.name));
+      const isOfficialDataset = group.resources.some((r) => r.name.startsWith("nextstrain/"));
       return isOfficialDataset
         ? { src: nextstrainLogoSmall.src, alt: "nextstrain logo" }
         : undefined;
     },
   });
-}
-
-/**
- * Returns whether a dataset belongs to the "nextstrain" collection.
- *
- * This checks against other collection prefixes because the leading
- * "nextstrain/" prefix is removed from datasets in the "nextstrain"
- * collection.¹
- *
- * ¹ docstring of NextcladeSource in src/sources/nextclade.js
- */
-function isNextstrain(name: string): boolean {
-  for (const collection of NON_NEXTSTRAIN_COLLECTIONS) {
-    if (name.startsWith(`${collection}/`)) {
-      return false;
-    }
-  }
-  return true;
 }
